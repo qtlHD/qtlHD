49e1e5ab69be25028612ff926bc0855f2eb7c501
Pjotr Prins <pjotr.public01@thebird.nl>
2011-06-08 14:02:33 +0200

    Marker primitive

--- src/D/core/qtl_objects.d
+++ src/D/core/qtl_objects.d
@@ -15,26 +15,37 @@ module qtl.qtl_object;
  */
 
 class Attribute {
-  private string description;
+  private string description = "core attribute";
 }
 
 /** 
  * The Marker struct is the most primitive representation of a marker, i.e.
  * the marker ID, the position, a reference to the marker name, and a reference
  * to a list of attributes - where an attribute can be any object. The
- * attrib_list gives a flexible way of tracking state.
+ * attrib_list gives a flexible way of tracking state. Arguably chromosome and 
+ * name are attributes, but they are pretty standard.
  *
- * The Marker object does not auto
+ * The Marker object does not keep track of the parent container.
  */
 
 struct Marker {
   const uint id;            /// Unique identifier
+  const string name;        /// Marker name
   const int chromosome;
-  double position;   /// The marker position - content depends on map
+  double position;          /// Marker position - content depends on map
   Attribute[] attrib_list;
-  unittest {
-    Marker m = { id:1, position:4.6};	      
-  }
 }
 
+/**
+ * Unit tests for primitives 
+ */
+
+unittest {
+  Marker m1 = { id:1, position:4.6};
+  assert(m1.id == 1);
+  assert(m1.attrib_list == null);
+  assert(m1.attrib_list.length == 0);
+  Marker m2 = { id:2, position:4.8, chromosome:1, attrib_list:new Attribute[1]};
+  assert(m2.attrib_list.length == 1);
+}
 
