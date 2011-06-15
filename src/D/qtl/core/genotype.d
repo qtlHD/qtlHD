8d44016ac14529733dca1affcd3120fdafd10942
Pjotr Prins <pjotr.public01@thebird.nl>
2011-06-15 13:25:24 +0200

    Genotype: Safe type conversion from string. Error message is now
      object.Exception@qtl/core/genotype.d(31): Unknown genotype H for RIL

--- src/D/qtl/core/genotype.d
+++ src/D/qtl/core/genotype.d
@@ -15,30 +15,64 @@ enum F2  { NA, A, H, B, HorB, HorA };
 enum BC  { NA, A, H };
 
 Genotype!T set_genotype(T)(in string s) {
-  // writeln(s);
   Genotype!T g;
-  switch(s) {
-    case "-":
-      g.value = T.NA;
-      break;  
-    case "A":
-      g.value = T.A;
-      break;
-    case "H":
-      g.value = T.H;
-      break;
-    case "B":
-      g.value = T.B;
-      break;
-    case "C":
-      g.value = T.HorB;
-      break;
-    case "D":
-      g.value = T.HorA;
-      break;
+  static if (T.stringof == "RIL") {
+    switch(s) {
+      case "-":
+        g.value = T.NA;
+        break;  
+      case "A":
+        g.value = T.A;
+        break;
+      case "B":
+        g.value = T.B;
+        break;
+      default:
+        throw new Exception("Unknown genotype " ~ s ~ " for " ~ T.stringof);
+    }
+  }
+  static if (T.stringof == "BC") {
+    switch(s) {
+      case "-":
+        g.value = T.NA;
+        break;  
+      case "A":
+        g.value = T.A;
+        break;
+      case "H":
+        g.value = T.H;
+        break;
+      default:
+        throw new Exception("Unknown genotype " ~ s ~ " for " ~ T.stringof);
+    }
+  }
+  static if (T.stringof == "F2") {
+    switch(s) {
+      case "-":
+        g.value = T.NA;
+        break;  
+      case "A":
+        g.value = T.A;
+        break;
+      case "H":
+        g.value = T.H;
+        break;
+      case "B":
+        g.value = T.B;
+        break;
+      case "C":
+        g.value = T.HorB;
+        break;
+      case "D":
+        g.value = T.HorA;
+        break;
+      default:
+        throw new Exception("Unknown genotype " ~ s ~ " for " ~ T.stringof);
+    }
   }
   return g;
 }
+
 
 
 unittest {
@@ -56,4 +90,16 @@ unittest {
   assert(gs[3].value == F2.H);
   assert(gs[4].value == F2.HorB);
   assert(gs[5].value == F2.HorA);
+}
+
+unittest {
+  Genotype!RIL[] ril;
+  ril ~= set_genotype!RIL("-");
+  ril ~= set_genotype!RIL("A");
+  ril ~= set_genotype!RIL("B");
+  // ril ~= set_genotype!RIL("H"); raises exception
+  // ril ~= set_genotype!RIL("C"); raises exception
+  assert(ril[0].value == RIL.NA);
+  assert(ril[1].value == RIL.A);
+  assert(ril[2].value == RIL.B);
 }
