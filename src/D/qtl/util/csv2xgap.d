57b2fe937b9ee317b5293aa6cd6df34616d9050d
Pjotr Prins <pjotr.public01@thebird.nl>
2011-06-25 14:08:58 +0200

    Build csv2xgap utility

--- src/D/qtl/util/csv2xgap.d
+++ src/D/qtl/util/csv2xgap.d
@@ -4,9 +4,26 @@
 
 module qtl.util.csv2xgap;
 
+import qtl.core.primitives;
+import qtl.core.chromosome;
+import qtl.core.phenotype;
+import qtl.core.genotype;
+import qtl.core.xgap;
+import qtl.plugins.input.read_csvr;
+import qtl.plugins.output.write_xgapbin;
+
+import std.stdio;
+import std.conv;
+import std.string;
+import std.path;
+import std.file;
+
+
 void main(string[] args){
   if(args.length != 3){
     writeln("Usage: convert in.csvr out.xbin");
+    writeln("  e.g. csv2xgap ../../test/data/input/multitrait.csvr test.xbin");
+
   }else{
     writeln("reading CSVR (" ~ args[1] ~ ") to XBIN (" ~ args[2] ~ ")");
     auto data = new CsvrReader!RIL(args[1]);
