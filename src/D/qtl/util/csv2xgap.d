a80ce6dacd94cc42640aa3daf0eb8f41cfac18c7
DannyArends <danny.arends@gmail.com>
2011-06-26 01:30:04 +0200

    csv2xgap now reads csv, csvr and supports RIL, F2 and BC

--- src/D/qtl/util/csv2xgap.d
+++ src/D/qtl/util/csv2xgap.d
@@ -10,6 +10,8 @@ import qtl.core.phenotype;
 import qtl.core.genotype;
 import qtl.core.xgap;
 import qtl.plugins.input.read_csvr;
+import qtl.plugins.input.read_csv;
+import qtl.plugins.input.read_interface;
 import qtl.plugins.output.write_xgapbin;
 
 import std.stdio;
@@ -18,17 +20,39 @@ import std.string;
 import std.path;
 import std.file;
 
+void print_help(){
+    writeln("Usage: convert type in.csv out.xbin");
+    writeln("Supported input files: CSV, CSVr");
+    writeln("Supported cross types: RIL, F2, BC");
+    writeln("  e.g. csv2xgap RIL ../../test/data/input/multitrait.csvr multitrait.xbin");
+    writeln("  e.g. csv2xgap F2 ../../test/data/input/hyper.csv hyper.xbin");
+}
+/*
+ * Function that creates a reader (csv or csvr) and provides a template for using different genotype enums
+ */
+void read_type(XType)(string filein, string fileout){
+  if(filein.lastIndexOf(".") > 0){
+    string extension = filein[filein.lastIndexOf(".")+1..$];
+    GenericReader!XType data;
+    if(extension.tolower() == "csv"){
+      data = cast(GenericReader!XType)new ReadSimpleCSV!XType(filein);
+    }
+    if(extension.tolower() == "csvr"){
+      data = cast(GenericReader!XType)new CSVrReader!XType(filein);
+    }
+    writeln("reading CSVR (" ~ filein ~ ") to XBIN (" ~ fileout ~ ")");
+    auto result = new BinaryWriter!(GenericReader!XType,XType)(data,fileout);
+    writefln("Reduced from: %.2f Kb to %.2f Kb", toKb(filein), toKb(fileout));
+  }
+}
 
 void main(string[] args){
-  if(args.length != 3){
-    writeln("Usage: convert in.csvr out.xbin");
-    writeln("  e.g. csv2xgap ../../test/data/input/multitrait.csvr test.xbin");
-
+  if(args.length != 4){
+    print_help();
   }else{
-    writeln("reading CSVR (" ~ args[1] ~ ") to XBIN (" ~ args[2] ~ ")");
-    auto data = new CsvrReader!RIL(args[1]);
-    auto result = new BinaryWriter!(CsvrReader!RIL,RIL)(data,args[2]);
-    writefln("Reduced from: %.2f Kb to %.2f Kb", toKb(args[1]), toKb(args[2]));
+    if(args[1] == "RIL") read_type!RIL(args[2],args[3]);
+    if(args[1] == "F2") read_type!F2(args[2],args[3]);
+    if(args[1] == "BC") read_type!F2(args[2],args[3]);
   }
 }
 
