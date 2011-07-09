10bde33d1df79f39ad868654f9031f68310eedaa
Pjotr Prins <pjotr.public01@thebird.nl>
2011-07-09 11:24:15 +0200

    chromosome.d: got rid of M in favor of Ms

--- src/D/qtl/core/chromosome.d
+++ src/D/qtl/core/chromosome.d
@@ -53,7 +53,7 @@ static bool is_sex(Chromosome chromosome) {
  */
 
 
-Tuple!(Chromosome,Ms)[] get_markers_by_chromosome(in Ms markers) {
+Tuple!(Chromosome,Ms)[] get_markers_by_chromosome(Ms)(in Ms markers) {
   Ms[string] alist;
   foreach(m ; markers.list) {
     if ((m.chromosome.name) !in alist) {
