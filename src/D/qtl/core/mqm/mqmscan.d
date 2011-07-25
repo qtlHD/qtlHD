b34d80ee29297adb38d5ddf3e639754934bff8fa
DannyArends <Danny.Arends@gmail.com>
2011-07-25 12:03:18 +0200

    MQM: Added empty files to hold the mqm functionality, already did the R bindings like we did lapack (static libs on Linux,MacOSX and using shared libs on Win32)

---
+++ src/D/qtl/core/mqm/mqmscan.d
@@ -0,0 +1,17 @@
+/**
+ * Multiple QTL modelling and scanning routine
+ **/
+
+module qtl.core.mqm.mqmscan;
+ 
+import std.container;
+import qtl.core.primitives;
+import std.stdio;
+import std.algorithm;
+import std.math;
+import std.string;
+import qtl.core.genotype;
+import qtl.plugins.input.read_csv;
+
+//Load the R-bindings
+import qtl.core.mqm.mqmutils;
