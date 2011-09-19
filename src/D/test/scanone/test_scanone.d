347672dd4c074ac2dc0a41f67d6a5cc31873c9a5
Karl Broman <kbroman@gmail.com>
2011-09-19 14:24:58 -0500

    Changes just to text printed in test_scanone.d

--- src/D/test/scanone/test_scanone.d
+++ src/D/test/scanone/test_scanone.d
@@ -106,15 +106,15 @@ unittest {
   auto c1 = find!("a[0].name == \"1\"")(c_mslist)[0];
   auto ms1 = c1[1].sort;
   writeln(ms1);
-  writeln("** Chromosome 1 **");
+  writeln(" -- Chromosome 1");
   auto rfs = recombination_fractions(ms1);
   assert(rfs.length==12);
   assert(to!string(rfs[0])[0..9]=="0.0098688");
   assert(to!string(rfs[1])=="0.189685");
   // Chromosome X
-  writeln("** Chromosome X **");
-  writeln("msx.length: ", msx.length);
-  writeln(recombination_fractions(msx));
+  writeln(" -- Chromosome X");
+  writeln("    msx.length: ", msx.length);
+  writeln("    ", recombination_fractions(msx));
   assert(to!string(recombination_fractions(msx)[0])=="0.285633");
   // expand map for each chromosome
   double totalexpsize = 0.0;
@@ -127,7 +127,7 @@ unittest {
   assert(to!string(totalexpsize) == "1104.29",to!string(totalexpsize));
  
   // test rfs after expansion
-  writeln("** Chr 1 with pseudomarkers **");
+  writeln(" -- Chr 1 with pseudomarkers");
   auto msin1 = new Markers!Marker(ms1);
   auto expanded_ms1 = add_stepped_markers_autosome(msin1,2.5,0);
   auto expanded_recombfs1 = recombination_fractions(expanded_ms1.list);
