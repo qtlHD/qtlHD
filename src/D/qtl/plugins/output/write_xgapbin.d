7f18277d86a0546d7ee88b6f7f9fb16cfaa777e3
DannyArends <Danny.Arends@gmail.com>
2011-10-31 15:37:07 +0100

    Updates, still not able to put in empty row/col names

--- src/D/qtl/plugins/output/write_xgapbin.d
+++ src/D/qtl/plugins/output/write_xgapbin.d
@@ -80,6 +80,26 @@ class BinaryWriter(Reader, XType) {
     }
     header.size = cast(int) (XgapMatrixHeader.sizeof + (elementsizes.length*4) + datasize);
     myWrite([header],outfile);
+    //Here we need to write rownames and colnames, Now STUBS
+   /* int[] rowlength;
+    string[] rownames;
+    for(auto r=0;r<header.nrow;r++){
+      rowlength ~= 0;
+      rownames ~= "";
+    }
+    myWrite(rowlength,outfile);
+    myWrite(rownames,outfile);
+    
+    int[] collength;
+    string[] colnames;
+    for(auto c=0;c<header.ncol;c++){
+      collength ~= 0;
+      colnames ~= "";
+    }
+
+    myWrite(collength,outfile);
+    myWrite(colnames,outfile);
+    */
     myWrite(elementsizes,outfile);
     foreach(T[] e;towrite){
       myWrite(e,outfile,t);
