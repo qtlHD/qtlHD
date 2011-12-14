b219f7c32ee611508bf56de84dc69e920df5cf40
Pjotr Prins <pjotr.public01@thebird.nl>
2011-12-14 13:54:28 +0100

    qtab: Stub for writing genotypes

--- src/D/qtl/plugins/output/write_qtab.d
+++ src/D/qtl/plugins/output/write_qtab.d
@@ -17,10 +17,34 @@ import std.path;
 import std.file;
 
 /**
+ * Write a tabular qtlHD genotype file. 
+ */
+
+void write_genotype_qtab(File f, string descr, in Individuals individuals, in Marker[] markers) {
+  f.writeln("# --- qtlHD-in-0.1 Genotype " ~ descr);
+  f.writeln("# --- Data Genotypes begin");
+  // write markers as a comment
+  f.write("#");
+  foreach(m; markers) {
+    f.write("\t",m.name);
+  }
+  f.writeln;
+  foreach(i, ind; individuals.list) {
+    f.write(ind.name);
+    // foreach(p; phenotypes[i]) {
+    //  f.write("\t",p.toString);
+    // }
+    f.writeln;
+  }
+  f.writeln("# --- Data Genotypes end");
+}
+
+/**
  * Write a tabular qtlHD phenotype file. 
  */
 
-void write_phenotype_qtab(P)(File f, in Individuals individuals, in string[] phenotypenames, in P[][] phenotypes) {
+void write_phenotype_qtab(P)(File f, string descr, in Individuals individuals, in string[] phenotypenames, in P[][] phenotypes) {
+  f.writeln("# --- qtlHD-in-0.1 Phenotype " ~ descr);
   // first we write the names and types
   f.writeln("# --- Type Phenotypes begin");
   foreach(n; phenotypenames) {
@@ -29,7 +53,7 @@ void write_phenotype_qtab(P)(File f, in Individuals individuals, in string[] phe
   f.writeln("# --- Type Phenotypes end");
 
   f.writeln("# --- Data Phenotypes begin");
-  // write markers as a comment
+  // write phenotype names as a comment
   f.write("#");
   foreach(n; phenotypenames) {
     f.write("\t",n);
@@ -57,8 +81,11 @@ unittest {
   // write the phenotype file
   auto pheno_fn = to!string(buildPath(dir,"regression","test_phenotype.qtab"));
   auto f = File(pheno_fn,"w");
-  write_phenotype_qtab(f, data.individuals, data.phenotypenames, data.phenotypes);
+  write_phenotype_qtab(f, "Test", data.individuals, data.phenotypenames, data.phenotypes);
   f.close();
   // write the genotype file
-
+  auto geno_fn = to!string(buildPath(dir,"regression","test_genotype.qtab"));
+  f = File(geno_fn,"w");
+  write_genotype_qtab(f, "Test", data.individuals, data.markers);
+  f.close();
 }
