51fc42b69a4d9f5f2342dbfeb7ca28b917640fd2
DannyArends <Danny.Arends@gmail.com>
2012-02-14 13:13:59 +0100

    Allow phenotypes without a .0 to be read as doubles

--- src/D/qtl/core/phenotype.d
+++ src/D/qtl/core/phenotype.d
@@ -7,14 +7,20 @@ module qtl.core.phenotype;
 import qtl.core.primitives;
 import std.conv;
 import std.stdio;
+import std.string;
 
 Phenotype!T set_phenotype(T)(in string s) {
   // writeln(s);
   Phenotype!T p;
-  if(s == "NA" || s == "-") 
+  if(s == "NA" || s == "-"){
     p.value = to!T(PHENOTYPE_NA);
-  else
-    p.value = to!T(s);
+  }else{
+    if(s.indexOf(".") != -1){
+      p.value = to!T(s);
+    }else{
+      p.value = to!T(s~".0");
+    }
+  }
   return p;
 }
 
