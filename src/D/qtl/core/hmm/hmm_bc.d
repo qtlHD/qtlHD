8ea6b2e8da2c229236f67660bc7e23db1234e769
Karl Broman <kbroman@gmail.com>
2012-03-28 21:17:00 -0500

    allTrueGenoBC() function using TrueGenotype

--- src/D/qtl/core/hmm/hmm_bc.d
+++ src/D/qtl/core/hmm/hmm_bc.d
@@ -13,110 +13,19 @@ import qtl.core.genotype;
 import qtl.core.map.genetic_map_functions;
 
 // things for the unit tests 
-import std.path;
 import qtl.plugins.qtab.read_qtab;
 
-
-// marginal genotype probability
-double initBC(TrueGenotype tg)
+TrueGenotype[] allTrueGenoBC()
 {
-  return(-LN2);
-  //  throw new Exception("true_gen not among the possible true genotypes");
+  auto g1 = new TrueGenotype("0,0");
+  auto g2 = new TrueGenotype("1,0");
+  return [g1, g2];
 }
 
 unittest {
   writeln("Unit test " ~ __FILE__);
-  writeln("    unit test initBC");
-
-
-  assert(abs(init(gen[0]) - log(0.5)) < 1e-12);
-  assert(abs(init(gen[1]) - log(0.5)) < 1e-12);
-}
-
-// emission probability (marker genotype "penetrance")
-double emit(Gref obs_gen, BC true_gen, double error_prob) 
-{
-  if(error_prob < 0.0 || error_prob > 1.0)
-     throw new Exception("error_prob must be >= 0 and <= 1");
   
-  switch(obs_gen.value) {
-  case BC.NA: return(0.0);
-  case BC.A: case BC.H:
-    if(obs_gen.value == true_gen) {
-      return(log(1.0-error_prob));
-    } else {
-      return(log(error_prob));
-    }
-  default: 
-    throw new Exception("obs_gen.value not among the possible marker genotypes");
-  }
+  auto g = allTrueGenoBC();
+  writeln(g.length);
+  foreach(gv; g) writeln(gv);
 }
-
-unittest {
-  writeln("    unit test emit for BC");
-  auto gen = [BC.A, BC.H];
-  auto ogen = [set_genotype!BC("-"), set_genotype!BC("A"), set_genotype!BC("H")];
-  double err = 0.001;
-  
-  foreach(g; gen) {
-    assert(abs( emit(ogen[0], g, err) ) < 1e-12);
-  }
-  assert(abs( emit(ogen[1], gen[0], err) - log(1-err) ) < 1e-12);
-  assert(abs( emit(ogen[1], gen[1], err) - log(err) ) < 1e-12);
-  assert(abs( emit(ogen[2], gen[0], err) - log(err) ) < 1e-12);
-  assert(abs( emit(ogen[2], gen[1], err) - log(1-err) ) < 1e-12);
-}
-
-// transition probabilities 
-double step(BC true_gen_left, BC true_gen_right, 
-	      double rec_frac)
-{
-  if(true_gen_left != BC.A && true_gen_left != BC.H)
-    throw new Exception("true_gen_left not among the possible true genotypes");
-  if(true_gen_right != BC.A && true_gen_right != BC.H)
-    throw new Exception("true_gen_right not among the possible true genotypes");
-  if(rec_frac < 0.0 || rec_frac > 0.5)
-     throw new Exception("rec_frac must be >= 0 and <= 1");
-
-  if(true_gen_left == true_gen_right) {
-    return(log(1.0-rec_frac));
-  }
-  else {
-    return(log(rec_frac));
-  }
-}
-
-
-unittest {
-  writeln("    unit test step for BC");
-  auto gen = [BC.A, BC.H];
-  double rf = 0.01;
-  assert(abs( step(gen[0], gen[0], rf) - log(1-rf) ) < 1e-12);
-  assert(abs( step(gen[0], gen[1], rf) - log(rf) ) < 1e-12);
-  assert(abs( step(gen[1], gen[0], rf) - log(rf) ) < 1e-12);
-  assert(abs( step(gen[1], gen[1], rf) - log(1-rf) ) < 1e-12);
-}
-
-  
-double nrec(BC true_gen_left, BC true_gen_right)
-{
-  if(true_gen_left != BC.A && true_gen_left != BC.H) 
-    throw new Exception("true_gen_left not a valid genotype.");
-  if(true_gen_right != BC.A && true_gen_right != BC.H) 
-    throw new Exception("true_gen_right not a valid genotype.");
-
-  if(true_gen_left == true_gen_right) return(0.0);
-  else return(1.0);
-}
-
-unittest {
-  writeln("    unit test nrec for BC");
-  auto gen = [BC.A, BC.H];
-  assert(nrec(gen[0], gen[0]) == 0.0);
-  assert(nrec(gen[0], gen[1]) == 1.0);
-  assert(nrec(gen[1], gen[0]) == 1.0);
-  assert(nrec(gen[1], gen[1]) == 0.0);
-}
-
-
-
