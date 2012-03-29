93709ce04fd4a5206c5d1c390734c5837306bbb8
Karl Broman <kbroman@gmail.com>
2012-03-28 21:42:12 -0500

    Add init_BC and init_F2

--- src/D/qtl/core/hmm/hmm_bc.d
+++ src/D/qtl/core/hmm/hmm_bc.d
@@ -12,10 +12,11 @@ import qtl.core.primitives;
 import qtl.core.genotype;
 import qtl.core.map.genetic_map_functions;
 
-// things for the unit tests 
+// things for the unit tests
 import qtl.plugins.qtab.read_qtab;
 
-TrueGenotype[] allTrueGenoBC()
+// vector of true genotypes
+TrueGenotype[] allTrueGeno_BC()
 {
   auto g1 = new TrueGenotype(0,0);
   auto g2 = new TrueGenotype(1,0);
@@ -24,8 +25,26 @@ TrueGenotype[] allTrueGenoBC()
 
 unittest {
   writeln("Unit test " ~ __FILE__);
-  
-  auto g = allTrueGenoBC();
-  writeln(g.length);
-  foreach(gv; g) writeln(gv);
+
+  auto g = allTrueGeno_BC();
+
+  assert(g.length == 2);
+  assert(g[0] == new TrueGenotype(0,0));
+  assert(g[1] == new TrueGenotype(1,0));
+}
+
+// Pr(true genotype)
+double init_BC(TrueGenotype truegen)
+{
+  if(truegen != new TrueGenotype(0,0) &&
+     truegen != new TrueGenotype(1,0))
+    throw new Exception("truegen must be 0,0 or 1,0");
+
+  return(-LN2);
+}
+
+unittest {
+  auto g = allTrueGeno_BC();
+  foreach(gv; g)
+    assert(to!string(init_BC(gv)) == to!string(log(0.5)));
 }
--- src/D/qtl/core/hmm/hmm_f2.d
+++ src/D/qtl/core/hmm/hmm_f2.d
@@ -12,10 +12,10 @@ import qtl.core.primitives;
 import qtl.core.genotype;
 import qtl.core.map.genetic_map_functions;
 
-// things for the unit tests 
+// things for the unit tests
 import qtl.plugins.qtab.read_qtab;
 
-TrueGenotype[] allTrueGenoF2()
+TrueGenotype[] allTrueGeno_F2()
 {
   auto g1 = new TrueGenotype(0,0);
   auto g2 = new TrueGenotype(1,0);
@@ -23,10 +23,53 @@ TrueGenotype[] allTrueGenoF2()
   return [g1, g2, g3];
 }
 
+// phase-known version (for est.map)
+TrueGenotype[] allTrueGeno_F2PK()
+{
+  auto g1 = new TrueGenotype(0,0);
+  auto g2 = new TrueGenotype(1,0);
+  auto g3 = new TrueGenotype(0,1);
+  auto g4 = new TrueGenotype(1,1);
+  return [g1, g2, g3, g4];
+}
+
+
 unittest {
   writeln("Unit test " ~ __FILE__);
-  
-  auto g = allTrueGenoF2();
-  writeln(g.length);
-  foreach(gv; g) writeln(gv);
+
+  auto g = allTrueGeno_F2();
+  auto gPK = allTrueGeno_F2PK();
+
+  assert(g.length == 3);
+  assert(g[0] == new TrueGenotype(0,0));
+  assert(g[1] == new TrueGenotype(1,0));
+  assert(g[2] == new TrueGenotype(1,1));
+
+  assert(gPK.length == 4);
+  assert(gPK[0] == new TrueGenotype(0,0));
+  assert(gPK[1] == new TrueGenotype(1,0));
+  assert(gPK[2] == new TrueGenotype(0,1));
+  assert(gPK[3] == new TrueGenotype(1,1));
+}
+
+// Pr(true genotype)
+double init_F2(TrueGenotype truegen)
+{
+  auto g = allTrueGeno_F2();
+
+  if(truegen==g[0] || truegen==g[2]) // AA or BB
+    return(-2.0*LN2);
+
+  if(truegen==g[1]) // AB
+    return(-LN2);
+
+  throw new Exception("truegen not among possible true genotypes");
+}
+
+unittest {
+  auto g = allTrueGeno_F2();
+  double val[] = [log(0.25), log(0.5), log(0.25)];
+
+  foreach(i, gv; g)
+    assert(to!string(init_F2(gv)) == to!string(val[i]));
 }
