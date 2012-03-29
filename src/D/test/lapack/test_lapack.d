d98603e88a7e2563a072c9305bb5e40cb123d626
Pjotr Prins <pjotr.public01@thebird.nl>
2012-03-29 14:11:02 -0500

    Cleanup test_lapack.d

--- src/D/test/lapack/test_lapack.d
+++ src/D/test/lapack/test_lapack.d
@@ -25,6 +25,7 @@ Should produce
   3.0
  -3.0
 
+
 */
 
 module test.lapack.test_lapack;
@@ -37,8 +38,6 @@ import std.string;
 
 pragma(lib, "blas");
 pragma(lib, "lapack");
-
-void dgels_(char *trans, f_int *m, f_int *n, f_int *nrhs, f_double *a, f_int *lda, f_double *b, f_int *ldb, f_double *work, f_int *lwork, f_int *info);
 
 double m[9] = [
   3, 1, 3,
@@ -61,56 +60,19 @@ double y[3] = [
   0, 0, 0
 ];
 
-extern (C) enum CBLAS_ORDER
-    {CblasRowMajor=101, CblasColMajor=102 };
-extern (C) enum CBLAS_TRANSPOSE
-    {CblasNoTrans=111, CblasTrans=112, CblasConjTrans=113, AtlasConj=114};
-
-const char CblasRowMajor = 101;
-const char CblasNoTrans = 111;
-
 extern(C) {
-
-alias float f_float;
-alias double f_double;
-alias int f_int;
+  alias float f_float;
+  alias double f_double;
+  alias int f_int;
 }
 
 // From scid:
 extern (C) void dgemv_(char *trans, f_int *m, f_int *n, f_double *alpha, f_double *A, f_int *lda, f_double *x, f_int *incx, f_double *beta, f_double *y, f_int *incy, f_int trans_len);
 
-
-extern (C) void cblas_dgemv(int order, int TransA , 
-  int M , int N , double alpha , f_double *A , int lda ,
-  f_double *X , int incX , double beta , f_double *Y , int incY);
-
-extern (C) void gemv(char *trans, f_int m, f_int n, f_double alpha, f_double *A, f_int lda,
+// The D interface calls the C interface
+void gemv(char trans, f_int m, f_int n, f_double alpha, f_double *A, f_int lda,
 f_double *x, f_int incx, f_double beta, f_double *y, f_int incy) {
-    dgemv_(trans, &m, &n, &alpha, A, &lda, x, &incx, &beta, y, &incy, 1);
-}
-
-void dblas_dgemv(int order, int TransA , 
-  int M , int N , double alpha , double A[] , int lda ,
-  double X[] , int incX , double beta , double Y[] , int incY)
-{
-   writeln("order=",order);
-   writeln("TransA=",TransA);
-   writeln("M=",M);
-   writeln("N=",N);
-   writeln("X=",X);
-   writeln("A=",A);
-   writeln("Y=",Y);
-   writeln("lda=",lda);
-   writeln("incX=",incX);
-   writeln("beta=",beta);
-   writeln("incY=",incY);
-   double *x = X.ptr;
-   double *a = A.ptr;
-   double *y = Y.ptr;
-   writeln("calling into cblas_dgemv");
-   // dgemv_(order,TransA,M,N,alpha,a,lda,x,incX,beta,y,incY);
-   writeln("returned from cblas_dgemv");
-   writeln(Y);
+    dgemv_(&trans, &m, &n, &alpha, A, &lda, x, &incx, &beta, y, &incy, 1);
 }
 
 int main()
@@ -122,12 +84,19 @@ int main()
     putchar('\n');
   }
 
-  // gemv(CblasRowMajor, CblasNoTrans, 3, 3, 1.0, m, 3,
-	//      x, 1, 0.0, y, 1);
-  gemv(cast(char *)toStringz("N"), 3, 3, 1.0, m2.ptr, 3,
-	     x.ptr, 1, 0.0, y.ptr, 1);
+  gemv('N', 3, 3, 1.0, m2.ptr, 3, x.ptr, 1, 0.0, y.ptr, 1);
 
   for (i=0; i<3; ++i)  printf("%5.1f\n", y[i]);
 
+  writeln("
+Above output iss supposed to look like
+
+  3.0  1.0  3.0
+  1.0  5.0  9.0
+  2.0  6.0  5.0
+ -1.0
+  3.0
+ -3.0
+");
   return 0;
 }
