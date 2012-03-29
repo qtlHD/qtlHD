5ce1842a80ce497df47e812a39d1029dabcc5718
Karl Broman <kbroman@gmail.com>
2012-03-28 22:10:16 -0500

    Add step_BC

--- src/D/qtl/core/hmm/hmm_bc.d
+++ src/D/qtl/core/hmm/hmm_bc.d
@@ -26,6 +26,7 @@ TrueGenotype[] allTrueGeno_BC()
 unittest {
   writeln("Unit test " ~ __FILE__);
 
+  writeln("    unit test allTrueGeno for BC");
   auto g = allTrueGeno_BC();
 
   assert(g.length == 2);
@@ -44,7 +45,47 @@ double init_BC(in TrueGenotype truegen)
 }
 
 unittest {
+  writeln("    unit test init for BC");
+
   auto g = allTrueGeno_BC();
   foreach(gv; g)
     assert(to!string(init_BC(gv)) == to!string(log(0.5)));
+}
+
+// ln Pr(genotype at right marker | genotype at left marker)
+double step_BC(in TrueGenotype truegen_left, in TrueGenotype truegen_right, in double rec_frac)
+{
+  auto atg = allTrueGeno_BC();
+
+  if(truegen_left == atg[0]) {
+    if(truegen_right == atg[0]) // A -> A
+      return(log(1.0-rec_frac));
+    if(truegen_right == atg[1]) // A -> H
+      return(log(rec_frac));
+  }
+
+  if(truegen_left == atg[1]) {
+    if(truegen_right == atg[1]) // H -> H
+      return(log(1.0-rec_frac));
+    if(truegen_right == atg[0]) // H -> A
+      return(log(rec_frac));
+  }
+
+  throw new Exception("inputs not among the possible true genotypes");
+}
+
+unittest {
+  writeln("    unit test step for BC");
+  auto g = allTrueGeno_BC();
+  double rf = 0.01;
+
+  assert( to!string( step_BC(g[0], g[0], rf) ) ==
+          to!string( log((1-rf)) ));
+  assert( to!string( step_BC(g[0], g[1], rf) ) ==
+          to!string( log(rf) ));
+
+  assert( to!string( step_BC(g[1], g[0], rf) ) ==
+          to!string( log(rf) ));
+  assert( to!string( step_BC(g[1], g[1], rf) ) ==
+          to!string( log((1-rf)) ));
 }
