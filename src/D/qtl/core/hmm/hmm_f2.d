faf3b84bca4c30e6b1a01227cf18e6197ea2a3ad
Karl Broman <kbroman@gmail.com>
2012-03-28 21:58:05 -0500

    Add step_F2()

--- src/D/qtl/core/hmm/hmm_f2.d
+++ src/D/qtl/core/hmm/hmm_f2.d
@@ -36,6 +36,7 @@ TrueGenotype[] allTrueGeno_F2PK()
 
 unittest {
   writeln("Unit test " ~ __FILE__);
+  writeln("    unit test allTrueGeno for F2");
 
   auto g = allTrueGeno_F2();
   auto gPK = allTrueGeno_F2PK();
@@ -71,7 +72,7 @@ double init_F2PK(in TrueGenotype truegen)
 {
   auto g = allTrueGeno_F2PK();
 
-  if(truegen==g[0] || truegen==g[1] || 
+  if(truegen==g[0] || truegen==g[1] ||
      truegen==g[2] || truegen==g[3])
     return(-2.0*LN2);
 
@@ -79,6 +80,8 @@ double init_F2PK(in TrueGenotype truegen)
 }
 
 unittest {
+  writeln("    unit test init for F2");
+
   auto g = allTrueGeno_F2();
   auto gPK = allTrueGeno_F2PK();
   double val[] = [log(0.25), log(0.5), log(0.25)];
@@ -90,3 +93,60 @@ unittest {
     assert(to!string(init_F2PK(gv)) == to!string(val[0]));
 }
 
+// ln Pr(genotype at right marker | genotype at left marker)
+double step_F2(in TrueGenotype truegen_left, in TrueGenotype truegen_right, in double rec_frac)
+{
+  auto atg = allTrueGeno_F2();
+  if(truegen_left==atg[0]) {
+    if(truegen_right==atg[0]) // A -> A
+      return(2.0*log(1.0-rec_frac));
+    if(truegen_right==atg[1]) // A -> H
+      return(LN2 + log(1.0-rec_frac) + log(rec_frac));
+    if(truegen_right==atg[2]) // A -> B
+      return(2.0*log(rec_frac));
+  }
+  else if(truegen_left == atg[1]) {
+    if(truegen_right==atg[0] || // H -> A
+       truegen_right==atg[2]) // H -> B
+      return(log(rec_frac) + log(1.0-rec_frac));
+    if(truegen_right==atg[1]) // H -> H
+      return(log((1.0-rec_frac)^^2 + rec_frac^^2));
+  }
+  else if(truegen_left == atg[2]) {
+    if(truegen_right==atg[0]) // B -> A
+      return(2.0*log(rec_frac));
+    if(truegen_right==atg[1]) // B -> H
+      return(LN2 + log(1.0-rec_frac) + log(rec_frac));
+    if(truegen_right==atg[2]) // B -> B
+      return(2.0*log(1.0-rec_frac));
+  }
+
+  throw new Exception("inputs not among the possible true genotypes");
+}
+
+unittest {
+  writeln("    unit test step for F2");
+  auto g = allTrueGeno_F2();
+  double rf = 0.01;
+
+  assert( to!string( step_F2(g[0], g[0], rf) ) ==
+          to!string( log((1-rf)^^2) ));
+  assert( to!string( step_F2(g[0], g[1], rf) ) ==
+          to!string( log(2.0*rf*(1-rf)) ));
+  assert( to!string( step_F2(g[0], g[2], rf) ) ==
+          to!string( log(rf^^2) ));
+
+  assert( to!string( step_F2(g[1], g[0], rf) ) ==
+          to!string( log(rf*(1-rf)) ));
+  assert( to!string( step_F2(g[1], g[1], rf) ) ==
+          to!string( log(rf^^2 + (1-rf)^^2) ));
+  assert( to!string( step_F2(g[1], g[2], rf) ) ==
+          to!string( log(rf*(1-rf)) ));
+
+  assert( to!string( step_F2(g[2], g[0], rf) ) ==
+          to!string( log(rf^^2) ));
+  assert( to!string( step_F2(g[2], g[1], rf) ) ==
+          to!string( log(2.0*rf*(1-rf)) ));
+  assert( to!string( step_F2(g[2], g[2], rf) ) ==
+          to!string( log((1-rf)^^2) ));
+}
