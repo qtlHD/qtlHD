671a690f4bb3042e91873758782079897fbce754
Karl Broman <kbroman@gmail.com>
2012-03-29 23:29:59 -0500

    split test_hmm.d file into two (f2 and bc)

---
+++ src/D/test/hmm/test_hmm_f2.d
@@ -0,0 +1,202 @@
+/**
+ * test reading data and then apply HMM
+ * (intercross example: listeria)
+ */
+
+module test.hmm.test_hmm_f2;
+
+import std.math, std.stdio, std.path;
+import std.exception, std.conv;
+import std.random;
+import std.algorithm;
+
+import qtl.core.primitives;
+import qtl.core.phenotype, qtl.core.chromosome, qtl.core.genotype;
+import qtl.plugins.qtab.read_qtab;
+import qtl.core.map.make_map;
+import qtl.core.map.genetic_map_functions;
+import qtl.core.hmm.hmm_f2;
+
+
+unittest {
+  writeln("Unit test " ~ __FILE__);
+}
+
+unittest {
+  alias std.path.buildPath buildPath;
+  auto dir = to!string(dirName(__FILE__) ~ sep ~
+                       buildPath("..","..","..","..","test","data", "input", "listeria_qtab"));
+
+  // load founder info
+  auto founder_fn = to!string(buildPath(dir, "listeria_founder.qtab"));
+  writeln("reading ", founder_fn);
+  auto info = read_founder_settings_qtab(founder_fn);
+  assert(info["Cross"] == "F2");
+  assert(info["Phase"] == "unknown");
+
+  // load symbols
+  auto symbol_fn = to!string(buildPath(dir,"listeria_symbol.qtab"));
+  // First read symbol information (the GenotypeCombinators)
+  writeln("reading ",symbol_fn);
+  auto fs = File(symbol_fn,"r");
+  auto symbols = read_genotype_symbol_qtab(fs);
+
+  // Test working of symbols
+  assert(symbols.decode("A") == symbols.decode("AA"));
+  assert(to!string(symbols.decode("NA")) == "[NA]");
+  assert(to!string(symbols.decode("A")) == "[(0,0)]");
+  assert(to!string(symbols.decode("H")) == "[(0,1), (1,0)]");
+  assert(to!string(symbols.decode("B")) == "[(1,1)]");
+  assert(to!string(symbols.decode("HorA")) == "[(0,0), (0,1)]");
+  assert(to!string(symbols.decode("HorB")) == "[(0,1), (1,1)]");
+
+  // Read genotype matrix
+  auto genotype_fn = to!string(buildPath(dir,"listeria_genotype.qtab"));
+  writeln("reading ",genotype_fn);
+  auto fg = File(genotype_fn,"r");
+  auto ret = read_genotype_qtab(fg, symbols);
+  auto individuals = ret[0];
+  auto genotype_matrix = ret[1];
+
+  // Show the first individual and genotypes
+  assert(individuals.list.length == 120);
+  assert(individuals.list[15].name == "16");
+  assert(genotype_matrix[119].length == 133);
+
+  // by symbol
+  assert(genotype_matrix[0][0] == symbols.decode("B"));
+  assert(genotype_matrix[0][3] == symbols.decode("H"));
+
+  assert(genotype_matrix[15][0] == symbols.decode("H"));
+  assert(genotype_matrix[15][130] == symbols.decode("HorB"));
+  writeln("genotype_matrix[15][130]: ", genotype_matrix[15][130]);
+  writeln("genotype_matrix[15][130].length: ", genotype_matrix[15][130].length);
+  writeln("genotype_matrix[15][130].list.length: ", genotype_matrix[15][130].list.length);
+  foreach(g; genotype_matrix[15][130].list)
+    writeln("genotype_matrix[15][130].list[i]: ", g, " ", typeid(g));
+
+  assert(genotype_matrix[18][129] == symbols.decode("B"));
+  assert(genotype_matrix[18][130] == symbols.decode("NA"));
+  assert(genotype_matrix[18][131] == symbols.decode("A"));
+
+  writeln("genotype_matrix[18][130]: ", genotype_matrix[18][130]);
+  writeln("genotype_matrix[18][130].length: ", genotype_matrix[18][130].length);
+  writeln("genotype_matrix[18][130].list.length: ", genotype_matrix[18][130].list.length);
+  foreach(g; genotype_matrix[18][130].list)
+    writeln("genotype_matrix[18][130].list[i]: ", g, " ", typeid(g));
+
+  // by founders
+  assert(genotype_matrix[0][0].list[0].homozygous == true);
+  assert(genotype_matrix[0][0].list[0].heterozygous == false);
+  assert(genotype_matrix[0][0].list[0].founders[0] == 1);
+  assert(genotype_matrix[0][0].list[0].founders[1] == 1);
+
+  assert(genotype_matrix[0][3].list[0].homozygous == false);
+  assert(genotype_matrix[0][3].list[0].heterozygous == true);
+  assert(genotype_matrix[0][3].list[0].founders[0] == 0);
+  assert(genotype_matrix[0][3].list[0].founders[1] == 1);
+
+  assert(genotype_matrix[15][0].list[0].homozygous == false);
+  assert(genotype_matrix[15][0].list[0].heterozygous == true);
+  assert(genotype_matrix[15][0].list[0].founders[0] == 0);
+  assert(genotype_matrix[15][0].list[0].founders[1] == 1);
+
+  assert(genotype_matrix[18][129].list[0].homozygous == true);
+  assert(genotype_matrix[18][129].list[0].heterozygous == false);
+  assert(genotype_matrix[18][129].list[0].founders[0] == 1);
+  assert(genotype_matrix[18][129].list[0].founders[1] == 1);
+
+  assert(genotype_matrix[18][131].list[0].homozygous == true);
+  assert(genotype_matrix[18][131].list[0].heterozygous == false);
+  assert(genotype_matrix[18][131].list[0].founders[0] == 0);
+  assert(genotype_matrix[18][131].list[0].founders[1] == 0);
+
+  // reading phenotypes
+  auto pheno_fn = to!string(buildPath(dir,"listeria_phenotype.qtab"));
+  writeln("reading ",pheno_fn);
+  auto p_res = read_phenotype_qtab!(Phenotype!double)(pheno_fn);
+  Phenotype!double[][] pheno = p_res[0];
+
+  assert(pheno.length == 120);
+  foreach(p; pheno) assert(p.length == 1);
+
+  // 1st ind, 1st phenotype
+  assert(pheno[0][0].value == 118.317);
+  // 3rd ind, 1st phenotype
+  assert(pheno[2][0].value == 194.917);
+  assert(to!string(pheno[29][0]) == "NA"); // missing value
+
+  // Marker map reader
+  auto marker_map_fn = to!string(buildPath(dir,"listeria_marker_map.qtab"));
+  writeln("reading ",marker_map_fn);
+  auto markers = read_marker_map_qtab!(Marker)(marker_map_fn);
+  assert(markers.length == 133);
+  assert(markers[0].name == "D10M44");
+  assert(markers[0].chromosome.name == "1");
+  assert(markers[3].get_position == 40.4136);
+  assert(markers[128].name == "D19M117");
+  assert(markers[128].chromosome.name == "19");
+  assert(markers[128].get_position == 16.364);
+
+  // marker id == numeric index
+  foreach(i, m; markers) assert(m.id == i);
+
+  // test splitting up of markers into chromosomes
+  //    note: chromosomes not necessarily ordered
+  auto markers_by_chr = get_markers_by_chromosome(markers);
+  writeln("markers_by_chr.length: ", markers_by_chr.length);
+  writeln("markers_by_chr[0].length: ", markers_by_chr[0].length);
+  writeln("markers_by_chr[0][0].name: ", markers_by_chr[0][0].name);
+  writeln("markers_by_chr[0][1][0].name: ", markers_by_chr[0][1][0].name);
+  foreach(chr; markers_by_chr) {
+    writefln("%2s (%2d): %-9s (%3d)", chr[0].name, chr[1].length, chr[1][0].name, chr[1][0].id);
+    // check that markers within chromosome are in order:
+    //    contiguous ids; non-decreasing position
+    assert(chr[1][0].chromosome.name == chr[0].name);
+    for(auto i=1; i<chr[1].length; i++) {
+      assert(chr[1][i].id == chr[1][i-1].id+1);
+      assert(chr[1][i].get_position >= chr[1][i-1].get_position);
+      assert(chr[1][i].chromosome.name == chr[0].name);
+    }
+  }
+
+  // sorted chromosomes
+  writeln("sorted chromosomes: ");
+
+  auto markers_by_chr_sorted = sort_chromosomes_by_marker_id(markers_by_chr);
+  Marker[] pmap_stepped, pmap_minimal;
+  foreach(chr; markers_by_chr_sorted) {
+    writef("%2s (%2d): %-9s (%3d)", chr[0].name, chr[1].length, chr[1][0].name, chr[1][0].id);
+    // check that markers within chromosome are in order:
+    //    contiguous ids; non-decreasing position
+    assert(chr[1][0].chromosome.name == chr[0].name);
+    for(auto i=1; i<chr[1].length; i++) {
+      assert(chr[1][i].id == chr[1][i-1].id+1);
+      assert(chr[1][i].get_position >= chr[1][i-1].get_position);
+      assert(chr[1][i].chromosome.name == chr[0].name);
+    }
+
+    pmap_stepped = add_stepped_markers_autosome(chr[1], 5.0, 0.0);
+    pmap_minimal = add_minimal_markers_autosome(chr[1], 5.0, 0.0);
+    writefln("\tmarkers: %3d\tpmar (stepped): %3d\tpmar (minimal): %3d",
+             chr[1].length,
+             pmap_stepped.length - chr[1].length,
+             pmap_minimal.length - chr[1].length);
+  }
+
+  // test calc_geno_prob with listeria data
+  writeln("Test calc_geno_prob with listeria data, chr 4");
+  auto chr2_map = markers_by_chr_sorted[2][1];
+  sort(chr2_map); // sort in place
+  auto pmap_stepped_chr2 = add_stepped_markers_autosome(chr2_map, 1.0, 0.0);
+
+  double[] dist_cM;
+  foreach(i; 1..pmap_stepped_chr2.length)
+    dist_cM ~= pmap_stepped_chr2[i].get_position() - pmap_stepped_chr2[i-1].get_position();
+  auto rec_frac = dist_to_recfrac(dist_cM, GeneticMapFunc.Carter_Falconer);
+  writeln("dist_cM.length: ", dist_cM.length);
+  writeln("rec_frac.length: ", rec_frac.length);
+  writeln("pmap_stepped_chr2.length: ", pmap_stepped_chr2.length);
+
+  auto chr2probs = calc_geno_prob_F2(genotype_matrix, pmap_stepped_chr2, rec_frac, 0.001);
+}
