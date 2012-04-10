876445d03a56e5a1250bf6e5129437573c6ab421
Karl Broman <kbroman@gmail.com>
2012-04-10 14:10:40 -0500

    dgelsy is working!

--- src/D/qtl/core/scanone/linreg.d
+++ src/D/qtl/core/scanone/linreg.d
@@ -16,6 +16,7 @@
 module qtl.core.scanone.linreg;
 
 import std.algorithm;
+import std.math;
 import std.range;
 import std.c.stdio;
 import std.stdio;
@@ -35,13 +36,13 @@ version(Windows){
   private import qtl.core.util.windows;
   private import qtl.plugins.renv.libload;
 
-  extern (C) void function(f_double *x, f_int *n, f_int *p, f_double *y, f_int *ny,
-                           f_double *tol, f_double *b, f_double *rsd, f_double *qty,
-                           f_int *k, f_int *jpvt, f_double *qraux, f_double *work) dqrls_;
+  extern (C) void function(f_int *m, f_int *n, f_int *nrhs, f_double *A, f_int *lda,
+                           f_double *B, f_int *ldb, f_int *jpvt, f_double *rcond,
+                           f_int *rank, f_double *work, f_int *lwork, f_int *info) dgelsy_;
 
   static this(){
     HXModule lib = load_library("Rblas");
-    load_function(dqrls_)(lib,"dqrls_");
+    load_function(dqrls_)(lib,"dgelsy_");
     writeln("Loaded BLAS functionality");
   }
 
@@ -50,83 +51,116 @@ version(Windows){
   pragma(lib, "lapack");
 
   // The C interface from the SciD library:
-  extern (C) void dqrls_(f_double *x, f_int *n, f_int *p, f_double *y, f_int *ny,
-                         f_double *tol, f_double *b, f_double *rsd, f_double *qty,
-                         f_int *k, f_int *jpvt, f_double *qraux, f_double *work);
+  extern (C) void dgelsy_(f_int *m, f_int *n, f_int *nrhs, f_double *A, f_int *lda,
+                          f_double *B, f_int *ldb, f_int *jpvt, f_double *rcond,
+                          f_int *rank, f_double *work, f_int *lwork, f_int *info);
 }
 
-// The D interface is a D-ified call which calls the C interface dqrls_
-void qrls(f_double *x,     // [n x p] covariate matrix
-          f_int n,         // number of individuals
-          f_int p,         // number of covariates
-          f_double *y,     // [n x ny] outcome matrix
-          f_int ny,        // number of outcomes
-          f_double tol,    // tolerance for determining columns to omit
-          f_double *b,     // [p x ny] output matrix with coefficients
-          f_double *rsd,   // [n x ny] output matrix with residuals
-          f_double *qty,   // [n x ny] output matrix
-          f_int k,         // on output, the rank of x
-          f_int *jpvt,     // [p] vector of pivots
-          f_double *qraux, // [p] output vector
-          f_double *work)  // [2*p] vector
+// The D interface is a D-ified call which calls the C interface dgelsy_
+// see 
+void gelsy(f_int m,         // number of rows in A
+           f_int n,         // number of columns in A
+           f_int nrhs,      // number of right-hand sides (no. columns in B)
+           f_double *A,     // [m x n] covariate matrix
+           f_int lda,       // leading dimension of A [== m]
+           f_double *B,     // [m x nrhs] outcome matrix
+           f_int ldb,       // leading dimension of B [== m]
+           f_int *jpvt,     // n-vector to keep track of reordering of columns of A
+           f_double rcond,  // used to determine the effective rank of A (condition number < 1/rcond)
+           f_int *rank,      // on output, the rank of A
+           f_double *work,  // [lwork] vector of workspace
+           f_int lwork,    // dimension of work (should be >= max(mn+3n+1, 2*mn+nrhs
+           f_int *info)     // on output, =0 indicates success; =-i indicates ith argument had illegal value
 {
-  // see R-2.15.0/src/appl/dqrls.f
-  dqrls_(x, &n, &p, y, &ny, &tol, b, rsd, qty, &k, jpvt, qraux, work);
+  // see R-2.15.0/src/appl/dgelsy.f
+
+  dgelsy_(&m, &n, &nrhs, A, &lda, B, &ldb, jpvt, &rcond, rank, work, &lwork, info);
+
+  if(*info!=0) throw new Exception("dgelsy_: illegal value in argument " ~ to!string(*info));
 }
 
 
 // fit linear regression model and return residual sum of squares
-double[] lm_rss(double x[], int n, int p, double y[], int ny, double tol=1e-7)
+double[] calc_linreg_rss(double x[], int nrow, int ncolx, double y[], int ncoly, double tol=1e-7)
 {
-  auto b = new double[p*ny];
-  auto rsd = new double[n*ny];
-  auto qty = new double[n*ny];
-  auto jpvt = new int[p];
-  auto qraux = new double[p];
-  auto work = new double[2*p];
-  int k;
-  auto rss = new double[ny];
+  int nrhs=ncoly, lda=nrow, ldb=nrow, info, rank;
+  auto lwork = max(nrow*ncolx + 3*ncolx + 1, 2*nrow*ncolx+nrhs);
+  auto work = new double[lwork];
+  auto jpvt = new int[ncolx];
 
-  foreach(i; 0..n) jpvt[i] = i;  // keeps track of pivoted columns
+  foreach(i; 0..ncolx) jpvt[i] = i+1;  // keeps track of pivoted columns
 
-  qrls(x.ptr, n, p, y.ptr, ny, tol, b.ptr, rsd.ptr, qty.ptr, k, jpvt.ptr, qraux.ptr, work.ptr);
+  double rcond = tol;
 
-  foreach(i; 0..ny) {
-    rss[i] = 0.0;
-    foreach(j; 0..n)
-      rss[i] += rsd[j+i*n]^^2;
+  rank = info = -999;
+  gelsy(nrow, ncolx, nrhs, x.ptr, lda, y.ptr, ldb, jpvt.ptr, rcond, &rank, work.ptr, lwork, &info);
+
+  auto rss = new double[ncoly];
+  foreach(i; 0..ncoly) rss[i]=0.0; // fill with 0's
+
+  if(rank == ncolx) { // X is of full rank
+    auto row_index = 0;
+
+    // in each column of y:
+    //  first rank values = estimated coefficients
+    //  sum of squares of the rest gives RSS (residual sum of squares)
+    foreach(i; 0..ncoly) {  
+      foreach(j; rank..nrow)
+        rss[i] += y[row_index+j]^^2;
+      row_index += nrow;
+    }
+  }
+  else {
   }
 
   return rss;
 }
 
 
-// x matrix
-//         8    9    5
-//         5    2    1
-//         4    2    8
-//         3    2    1
-//         3    9    4
-//
-// y =     42   12   32   10   33
-//
-// resid =  0.42328 73370 96660 221114
-//         -0.33645 20175 62198 779643
-//         -0.05099 65851 27883 312105
-//         -0.20935 25681 23212 373630
-//         -0.29066 48546 93705 954070
+unittest {
+  writeln("Unit test " ~ __FILE__);
+  writeln("  --X matrix with full rank");
+
+  double[] x = [ 8, 5, 4, 3, 3,
+                 9, 2, 2, 2, 9,
+                 5, 1, 8, 1, 4];
+
+  double[] y = [42, 12, 32, 10, 33];
 
 
+  double rss_R = 0.42328733709665872231;
 
-// not full rank x matrix
-//         8    9   11    5    6
-//         5    2    9    1    5
-//         4    2    0    8    3
-//         3    2    5    1    2
-//         3    9    2    4    1
-//
-// resid =  0.42323 74179 03404 522512
-//         -0.33310 35233 49901 564643
-//         -0.05094 52447 47632 054128
-//         -0.21553 75739 32289 283629
-//         -0.28999 60085 63443 874451
+  int nrow = cast(int)y.length;
+  int ncolx = cast(int)x.length / nrow;
+  int ncoly = 1;
+
+  auto rss = calc_linreg_rss(x, nrow, ncolx, y, ncoly);
+
+  assert(abs(rss[0] - rss_R) < 1e-12);
+}
+
+/* this part isn't ready
+unittest {
+  writeln("Unit test " ~ __FILE__);
+  writeln("  --X matrix with full rank");
+
+  double[] x = [ 8, 5, 4, 3, 3,
+                 9, 2, 2, 2, 9,
+                 11,9, 0, 5, 2,
+                 5, 1, 8, 1, 4
+                 6, 5, 3, 2, 1];
+
+  double[] y = [42, 12, 32, 10, 33];
+
+
+  double rss_R = 0.42328733709665872231;
+
+  int nrow = cast(int)y.length;
+  int ncolx = cast(int)x.length / nrow;
+  int ncoly = 1;
+
+  auto rss = calc_linreg_rss(x, nrow, ncolx, y, ncoly);
+
+  assert(abs(rss[0] - rss_R) < 1e-12);
+}
+*/
