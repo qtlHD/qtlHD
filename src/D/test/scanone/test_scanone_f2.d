eda860043b7886b5d39ca3827a617795629c657b
Karl Broman <kbroman@gmail.com>
2012-04-17 01:12:15 -0500

    Add to test_scanone_f2 with listeria data
      - I spent a *very* long time on chromosome 13.  I'm glad I looked at it, as it
        includes the HorB case, which proved to be quite tricky.
    
      Important notes: 
    
        - R/qtl was using what appears to be incorrect emit_f2 probabilities for the
          HorB / HorA cases.
    
        - The matching of true and observed genotypes wasn't working for these cases
          as the true geno picks just one phase and the HorB/HorA cases included 
          just one of 0,1 or 1,0 and not both.

--- src/D/test/scanone/test_scanone_f2.d
+++ src/D/test/scanone/test_scanone_f2.d
@@ -86,7 +86,7 @@ unittest {
   sort(chr5_map); // sort in place
 
   // add pseudomarkers
-  auto chr5_map_wpmark = add_minimal_markers_autosome(chr5_map, 2);
+  auto chr5_map_wpmark = add_minimal_markers_autosome(chr5_map, 2.0);
 
   // calc_genoprob
   auto rec_frac = recombination_fractions(chr5_map_wpmark, GeneticMapFunc.Kosambi);
@@ -124,4 +124,82 @@ unittest {
   assert(lod.length == Rlod.length);
   foreach(i; 0..lod.length)
     assert(abs(lod[i][0] - Rlod[i]) < 1e-5);
+
+
+  // chr 12
+  auto chr12_map = markers_by_chr_sorted[11][1];
+  sort(chr12_map); // sort in place
+
+  // add pseudomarkers
+  auto chr12_map_wpmark = add_minimal_markers_autosome(chr12_map, 2.0);
+
+  // calc_genoprob
+  rec_frac = recombination_fractions(chr12_map_wpmark, GeneticMapFunc.Haldane);
+  auto chr12probs = calc_geno_prob_F2(genotype_matrix, chr12_map_wpmark, rec_frac, 0.02);
+
+  // run scanone and calculate LOD scores
+  rss = scanone_hk(chr12probs, pheno, addcovar, intcovar, weights);
+  rss0 = scanone_hk_null(pheno, addcovar, weights);
+  lod = rss_to_lod(rss, rss0, pheno.length);
+
+  /*******************************
+   * in R:
+
+   data(listeria)
+   listeria <- listeria["-X",]
+   listeria <- calc.genoprob(listeria, step=2, stepwidth="max", err=0.02, map="haldane")
+   out <- scanone(listeria, method="hk", chr=12)
+
+   *
+   ******************************/
+
+  Rlod = [0.4468656029654881, 0.3171983133512413, 0.1990813547934067, 0.1085411558419196, 0.0529134102031890, 0.0499288496749841,
+          0.0569520588662726, 0.0748122318320270, 0.1011755549142777, 0.1311198858168723, 0.1594622861026096, 0.1829074638146722,
+          0.2005413932699867, 0.3488392293048719, 0.5291124029305365, 0.7147266997326369, 0.8774348269768666, 0.9959712199683395,
+          1.1522059538291387, 1.3442422509452854, 1.5625548610798319, 1.7897661549670261, 2.0040604210789752, 2.1853767220865734,
+          2.2063171730782187, 2.1613428479913637, 2.0439351230456282, 1.8643517317593705, 1.6468400429176882, 1.4195839856907924,
+          1.2049975503663291];
+
+  assert(lod.length == Rlod.length);
+  foreach(i; 0..lod.length)
+    assert(abs(lod[i][0] - Rlod[i]) < 1e-6);
+
+  // chr 13
+  auto chr13_map = markers_by_chr_sorted[12][1];
+  sort(chr13_map); // sort in place
+
+  // add pseudomarkers
+  auto chr13_map_wpmark = add_minimal_markers_autosome(chr13_map, 2.5);
+
+  // calc_genoprob
+  rec_frac = recombination_fractions(chr13_map_wpmark, GeneticMapFunc.Haldane);
+  auto chr13probs = calc_geno_prob_F2(genotype_matrix, chr13_map_wpmark, rec_frac, 0.001);
+
+  // run scanone and calculate LOD scores
+  rss = scanone_hk(chr13probs, pheno, addcovar, intcovar, weights);
+  rss0 = scanone_hk_null(pheno, addcovar, weights);
+  lod = rss_to_lod(rss, rss0, pheno.length);
+
+  /*******************************
+   * in R:
+
+   data(listeria)
+   listeria <- listeria["-X",]
+   for(i in seq(along=listeria$geno))
+     listeria$geno[[i]]$map <- round(listeria$geno[[i]]$map, 4)
+   listeria <- calc.genoprob(listeria, step=2.5, stepwidth="max", err=0.001, map="haldane")
+   out <- scanone(listeria, method="hk", chr=13)
+
+   *
+   ******************************/
+
+  Rlod = [1.30859426838913, 1.63907750026016, 2.08904295511854, 2.52644926134673, 2.89361799872603, 3.14916884205684,
+          3.28383743561648, 3.52057919680880, 3.67436254196735, 3.67449099540994, 4.12740932094681, 4.38903263328922,
+          4.40806342365391, 3.88850412605399, 4.55939781788635, 4.62626215214118, 5.87536984413850, 4.56342340093875,
+          4.56365900796430, 3.97415853535017, 3.14762891112053, 2.26658588253167, 1.50094396044409];
+
+  assert(lod.length == Rlod.length);
+  foreach(i; 0..lod.length)
+    assert(abs(lod[i][0] - Rlod[i]) < 5e-4);
+
 }
