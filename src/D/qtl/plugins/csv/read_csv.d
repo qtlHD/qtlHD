9c7a2fe6b85df37b8257b20fdecec51c5f761450
DannyArends <Danny.Arends@gmail.com>
2012-04-25 10:24:59 +0200

    read_csv.d: std.path.sep is deprecated, changed to dirSeparator

--- src/D/qtl/plugins/csv/read_csv.d
+++ src/D/qtl/plugins/csv/read_csv.d
@@ -138,7 +138,7 @@ class ReadSimpleCSV(XType,ObservedXType) {
 unittest {
   writeln("Unit test " ~ __FILE__);
   alias std.path.buildPath buildPath;
-  auto fn = to!string(dirName(__FILE__) ~ sep ~ buildPath("..","..","..","..","..","test","data","input","listeria.csv"));
+  auto fn = to!string(dirName(__FILE__) ~ dirSeparator ~ buildPath("..","..","..","..","..","test","data","input","listeria.csv"));
   writeln("  - reading CSV " ~ fn);
   Marker m2 = new Marker(4.8);
   auto markers = [ m2 ];
@@ -172,7 +172,7 @@ unittest {
 
 unittest {
   alias std.path.buildPath buildPath;
-  auto fn = to!string(dirName(__FILE__) ~ sep ~ buildPath("..","..","..","..","..","test","data","input","hyper.csv"));
+  auto fn = to!string(dirName(__FILE__) ~ dirSeparator ~ buildPath("..","..","..","..","..","test","data","input","hyper.csv"));
   writeln("  - reading CSV " ~ fn);
   auto data = new ReadSimpleCSV!(F2,ObservedF2)(fn);
   assert(data.markers.length == 174, to!string(data.markers.length));
@@ -200,7 +200,7 @@ unittest {
 
 unittest {
   alias std.path.buildPath buildPath;
-  auto fn = to!string(dirName(__FILE__) ~ sep ~ buildPath("..","..","..","..","..","test","data","input","hyper_noX.csv"));
+  auto fn = to!string(dirName(__FILE__) ~ dirSeparator ~ buildPath("..","..","..","..","..","test","data","input","hyper_noX.csv"));
   writeln("  - reading CSV " ~ fn);
   auto data = new ReadSimpleCSV!(BC,ObservedBC)(fn);
   assert(data.markers.length == 170, to!string(data.markers.length));
@@ -249,7 +249,7 @@ GENOTYPE D as 1,1 0,1";
     observed.symbols ~= legaltype;
   }
   alias std.path.buildPath buildPath;
-  auto fn = to!string(dirName(__FILE__) ~ sep ~ buildPath("..","..","..","..","..","test","data","input","listeria.csv"));
+  auto fn = to!string(dirName(__FILE__) ~ dirSeparator ~ buildPath("..","..","..","..","..","test","data","input","listeria.csv"));
   writeln("  - reading CSV (for Flex) " ~ fn);
   auto data = new ReadSimpleCSV!(Flex,ObservedFlex)(fn, observed);
   auto cross = data.crosstype;
