212277d18d5ed9edc85b4ea59028b370cd99972f
Pjotr Prins <pjotr.public01@thebird.nl>
2012-05-14 19:14:48 +0200

    Version and usage

--- src/D/qtl/main/scanone.d
+++ src/D/qtl/main/scanone.d
@@ -4,21 +4,32 @@ import std.getopt;
 import std.stdio;
 import std.conv;
 import std.exception;
+import std.file;
+import std.string;
 
-immutable usage = "
-scanone qtlHD $version (c) 2012
+static string ver = import("VERSION");
 
-  usage:
+string copyright = "; qtlHD project (c) 2012";
+string usage = "
+  usage: scanone [options] inputfile(s)
 
+  options:
+
+    -v --verbosity    Set verbosity level (default 1)
+
+  examples:
+
+    scanone listeria_symbol.qtab listeria_founder.qtab listeria_marker_map.qtab listeria_genotype.qtab listeria_phenotype.qtab 
 ";
 
 int main(string[] args) {
+  writeln("scanone ",strip(ver)," ",copyright);
   if (args.length == 1) {
     writeln(usage);
     return 0;
   }
   uint verbosity = 1;
-  getopt(args, "v|verbosity", delegate (string o, string v) { verbosity = to!int(v); } );
+  getopt(args, "v|verbosity", (string o, string v) { verbosity = to!int(v); } );
 
   writeln("Verbosity ",verbosity);
   return 0;
