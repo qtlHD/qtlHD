ffca43be6925010b6846c8f047c0a15f0a46ec89
Pjotr Prins <pjotr.public01@thebird.nl>
2012-05-14 18:42:16 +0200

    std.getopt

--- src/D/qtl/main/scanone.d
+++ src/D/qtl/main/scanone.d
@@ -1,5 +1,26 @@
 // Entry point for CLI scanone tool
 
-int main() {
+import std.getopt;
+import std.stdio;
+import std.conv;
+import std.exception;
+
+immutable usage = "
+scanone qtlHD $version (c) 2012
+
+  usage:
+
+";
+
+int main(string[] args) {
+  if (args.length == 1) {
+    writeln(usage);
+    return 0;
+  }
+  uint verbosity = 1;
+  getopt(args, "v|verbosity", 
+    delegate void(string o, string v) { verbosity = to!int(v); return; } );
+
+  writeln("Verbosity ",verbosity);
   return 0;
 }
