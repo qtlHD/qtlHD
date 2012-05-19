330ec0275edfbe57876037132775403d9f6cdc88
Karl Broman <kbroman@gmail.com>
2012-05-18 23:05:00 -0500

    Revise simulate/meiosis.d to use my new RNG functions

--- src/D/qtl/core/simulate/meiosis.d
+++ src/D/qtl/core/simulate/meiosis.d
@@ -4,21 +4,26 @@
 
 module qtl.core.simulate.meiosis;
 
-import std.algorithm, std.stdio, std.string, std.conv;
-import qtl.core.simulate.rlib;
+import math.distributions.random;
+import std.algorithm;
+import std.stdio;
+import std.string;
+import std.conv;
+import std.random;
+import std.math;
 
 // simulate crossover locations on a chromosome under no interference
 // chrlen = chromosome length in cM
-double[] meiosisNI(double chrlen)
+double[] meiosisNI(double chrlen, ref Random gen)
 in {
   assert(chrlen > 0, "chrlen must be > 0");
 }
 body {
-  int n_xo = rpois(chrlen/100.0);
+  int n_xo = rpois(chrlen/100.0, gen);
 
   auto result = new double[](n_xo);
   foreach(i; 0..n_xo)
-    result[i] = unif_rand()*chrlen;
+    result[i] = uniform(0.0, chrlen, gen);
 
   sort(result);
   return(result);
@@ -28,9 +33,7 @@ body {
 
 // m = interference parameter
 // p = proportion of crossovers coming from no interference model
-
-
-double[] meiosis(double chrlen, int m=0, double p=0.0)
+double[] meiosis(double chrlen, int m, double p, ref Random gen)
 in {
   assert(chrlen > 0, "chrlen must be > 0");
   assert(m >= 0, "m must be >= 0");
@@ -38,12 +41,12 @@ in {
 }
 body {
   // no interference case is simple
-  if(m==0 || p==0) return(meiosisNI(chrlen));
+  if(m==0 || p==0) return(meiosisNI(chrlen, gen));
 
   // number of crossovers and intermediates (on 4-strand bundle)
-  int n_points_interf = rpois(chrlen*(m+1)/50.0*(1.0-p) );
+  int n_points_interf = rpois(chrlen*(m+1)/50.0*(1.0-p), gen );
   // number of crossovers from no interference mechanism (on final product)
-  int n_points_ni = rpois(chrlen/100.0*p);
+  int n_points_ni = rpois(chrlen/100.0*p, gen);
 
   double[] result;
   result.reserve(n_points_interf/(m+1) + n_points_ni);
@@ -51,34 +54,35 @@ body {
   // locations of crossovers and intermediates
   auto pts_interf = new double[](n_points_interf);
   foreach(i; 0..n_points_interf)
-    pts_interf[i] = unif_rand()*chrlen;
+    pts_interf[i] = uniform(0.0, chrlen, gen);
   sort(pts_interf);
 
   // every (m+1)st point is chiasma; thin to give crossovers
-  int first = cast(int)((m+1)*unif_rand());
+  int first = cast(int)uniform(0.0, m+1, gen);
   for(auto i=first; i<n_points_interf; i+= m+1)
-    if(unif_rand() < 0.5) result ~= pts_interf[i];
+    if(uniform(0.0, 1.0, gen) < 0.5) result ~= pts_interf[i];
 
   auto pts_ni = new double[](n_points_ni);
   foreach(i; 0..n_points_ni)
-    result ~= unif_rand()*chrlen;
+    result ~= uniform(0.0, chrlen, gen);
 
   sort(result);
   return(result);
 }
 
 unittest {
-  R_Init();
+  writeln("Unit test " ~ __FILE__);
+
+  Random gen;
+  gen.seed(unpredictableSeed);
 
   double[] product;
 
   foreach(m; 0..10) {
-    product = meiosis(200.0, m, 0.05);
+    product = meiosis(200.0, m, 0.05, gen);
     writeln("No. crossovers = ", product.length);
     foreach(p; product)
       writef("%6.3f ", p);
     writeln();
   }
-
-  R_Close();
 }
