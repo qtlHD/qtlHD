bad3554335d449016cbfef3a7b3bf57544cb8478
Karl Broman <kbroman@gmail.com>
2012-05-20 18:28:58 -0500

    Rename some variables in qtl/core/simulate/meiosis

--- src/D/qtl/core/simulate/meiosis.d
+++ src/D/qtl/core/simulate/meiosis.d
@@ -13,68 +13,68 @@ import std.random;
 import std.math;
 
 // simulate crossover locations on a chromosome under no interference
-// chrlen = chromosome length in cM
-double[] meiosisNI(double chrlen, ref Random gen)
+// chrlen_cM = chromosome length in cM
+double[] meiosisNI(double chrlen_cM, ref Random gen)
 in {
-  assert(chrlen > 0, "chrlen must be > 0");
+  assert(chrlen_cM > 0, "chrlen_cM must be > 0");
 }
 body {
-  int n_xo = rpois(chrlen/100.0, gen);
+  int n_xo = rpois(chrlen_cM/100.0, gen);
 
-  auto result = new double[](n_xo);
+  auto xo_locations = new double[](n_xo);
   foreach(i; 0..n_xo)
-    result[i] = uniform(0.0, chrlen, gen);
+    xo_locations[i] = uniform(0.0, chrlen_cM, gen);
 
-  sort(result);
-  return(result);
+  sort(xo_locations);
+  return(xo_locations);
 }
 
 
 
 /*
  Simulate crossover locations on a chromosome using Stahl model
-   chrlen = chromosome length in cM
+   chrlen_cM = chromosome length in cM
    m = interference parameter
    p = proportion of crossovers coming from no interference model
 
  p=0 gives the chi-square model
  m=0 or p=1 gives the no-interference model
 */
-double[] meiosis(double chrlen, int m, double p, ref Random gen)
+double[] meiosis(double chrlen_cM, int m, double p, ref Random gen)
 in {
-  assert(chrlen > 0, "chrlen must be > 0");
+  assert(chrlen_cM > 0, "chrlen_cM must be > 0");
   assert(m >= 0, "m must be >= 0");
   assert(p >= 0 && p <= 1, "p must be in [0,1]");
 }
 body {
   // no interference case is simple
-  if(m==0 || p==1) return(meiosisNI(chrlen, gen));
+  if(m==0 || p==1) return(meiosisNI(chrlen_cM, gen));
 
   // number of crossovers and intermediates (on 4-strand bundle)
-  int n_points_interf = rpois(chrlen*(m+1)/50.0*(1.0-p), gen );
+  int n_points_interf = rpois(chrlen_cM*(m+1)/50.0*(1.0-p), gen );
   // number of crossovers from no interference mechanism (on final product)
-  int n_points_ni = rpois(chrlen/100.0*p, gen);
+  int n_points_ni = rpois(chrlen_cM/100.0*p, gen);
 
-  double[] result;
-  result.reserve(n_points_interf/(m+1) + n_points_ni);
+  double[] xo_locations;
+  xo_locations.reserve(n_points_interf/(m+1) + n_points_ni);
 
   // locations of crossovers and intermediates
   auto pts_interf = new double[](n_points_interf);
   foreach(i; 0..n_points_interf)
-    pts_interf[i] = uniform(0.0, chrlen, gen);
+    pts_interf[i] = uniform(0.0, chrlen_cM, gen);
   sort(pts_interf);
 
   // every (m+1)st point is chiasma; thin to give crossovers
   int first = cast(int)uniform(0.0, m+1, gen);
   for(auto i=first; i<n_points_interf; i+= m+1)
-    if(uniform(0.0, 1.0, gen) < 0.5) result ~= pts_interf[i];
+    if(dice(gen, 1, 1) < 0.5) xo_locations ~= pts_interf[i];
 
   auto pts_ni = new double[](n_points_ni);
   foreach(i; 0..n_points_ni)
-    result ~= uniform(0.0, chrlen, gen);
+    xo_locations ~= uniform(0.0, chrlen_cM, gen);
 
-  sort(result);
-  return(result);
+  sort(xo_locations);
+  return(xo_locations);
 }
 
 unittest {
