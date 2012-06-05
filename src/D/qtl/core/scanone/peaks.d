19ba84f486471be0c2f3e43236ff3c6879ebeccd
Karl Broman <kbroman@gmail.com>
2012-06-05 04:51:03 +0200

    Add unit test to qtl/core/scanone/peaks.d

--- src/D/qtl/core/scanone/peaks.d
+++ src/D/qtl/core/scanone/peaks.d
@@ -14,6 +14,7 @@ import std.typecons;
 import qtl.core.chromosome;
 import qtl.core.primitives;
 
+// find maximum lod score and the position at which it occurred
 Tuple!(double, Marker) get_peak_scanone(double[] lod, Marker[] map)
 in {
   assert(lod.length == map.length, "lod and map should have the same length");
@@ -27,6 +28,7 @@ body {
   return Tuple!(double, Marker)(lod[index[0]], map[index[0]]);
 }
 
+// same for doubly-index LOD scores
 Tuple!(double, Marker)[] get_peak_scanone(double[][] lod, Marker[] map)
 in {
   assert(lod.length == map.length, "lod and map should have the same length");
@@ -43,4 +45,24 @@ body {
   }
 
   return peaks;
+}
+
+unittest {
+  writeln("Unit test " ~ __FILE__);
+
+  import qtl.core.simulate.map;
+  import qtl.core.chromosome;
+
+  double[] lod = [1.0, 3.0, 2.3, 5.6, 1.8];
+
+  auto chromosome = get_chromosome_with_id("1");
+  auto map = generate_map_eqspacing_onechr(100.0, cast(uint)lod.length, 1, chromosome);
+
+  auto peak = get_peak_scanone(lod, map);
+  assert(peak[0] == 5.6 && peak[1].name == "m4");
+
+  double[][] lod_2d = [[4.0, 5.0], [5.0, 2.6], [4.9, 0.3], [4.9, 5.3], [2.8, 2.8]];
+  auto peak_2d = get_peak_scanone(lod_2d, map);
+  assert(peak_2d[0][0] == 5.0 && peak_2d[0][1].name == "m2");
+  assert(peak_2d[1][0] == 5.3 && peak_2d[1][1].name == "m4");
 }
