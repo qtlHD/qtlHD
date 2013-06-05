807f78796119fffd514f13273ec52a8a9f43256a
Karl Broman <kbroman@gmail.com>
2013-06-05 10:42:48 -0500

    HMM calcgenoprob for X chr

--- src/D/qtl/core/hmm/calcgenoprob.d
+++ src/D/qtl/core/hmm/calcgenoprob.d
@@ -14,11 +14,14 @@ import qtl.core.hmm.forwardbackward;
 import qtl.core.hmm.cross;
 
 // calculate QTL genotype probabilities
-double[][][] calc_geno_prob(Cross cross,
-                            GenotypeCombinator[][] genotypes,
-                            Marker[] marker_map,
-                            double[] rec_frac,
-                            double error_prob)
+double[][][] calc_geno_prob(T)(Cross cross,
+                               GenotypeCombinator[][] genotypes,
+                               bool is_X_chr,
+                               bool[] is_female,
+                               T[] cross_direction,
+                               Marker[] marker_map,
+                               double[] rec_frac,
+                               double error_prob)
 {
   if(marker_map.length != rec_frac.length+1) {
     throw new Exception("no. positions in marker map doesn't match rec_frac length");
@@ -29,6 +32,10 @@ double[][][] calc_geno_prob(Cross cross,
     if(rf < 0 || rf > 0.5)
       throw new Exception("rec_frac must be >= 0 and <= 0.5");
   }
+  if(is_female.length != genotypes.length)
+    throw new Exception("is_female should be same length as genotypes")
+  if(cross_direction.length != genotypes.length)
+    throw new Exception("cross_direction should be same length as genotypes")
 
   size_t n_individuals = genotypes.length;
   size_t n_positions = marker_map.length;
@@ -38,8 +45,8 @@ double[][][] calc_geno_prob(Cross cross,
   auto genoprobs = new double[][][](n_positions, n_individuals, cross.all_true_geno.length);
 
   foreach(ind; 0..n_individuals) {
-    alpha = forwardEquations(cross, genotypes[ind], marker_map, rec_frac, error_prob);
-    beta = backwardEquations(cross, genotypes[ind], marker_map, rec_frac, error_prob);
+    alpha = forwardEquations(cross, genotypes[ind], is_X_chr, is_female[ind], cross_direction[ind], marker_map, rec_frac, error_prob);
+    beta = backwardEquations(cross, genotypes[ind], is_X_chr, is_female[ind], cross_direction[ind], marker_map, rec_frac, error_prob);
 
     // calculate genotype probabilities
     double sum_at_pos;
