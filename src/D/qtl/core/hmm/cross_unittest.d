3e8cd24698dadef2f375afe89013ee7123e97a95
Karl Broman <kbroman@gmail.com>
2013-06-05 21:53:25 -0500

    Update cross_unittest.d with X chr stuff

--- src/D/qtl/core/hmm/cross_unittest.d
+++ src/D/qtl/core/hmm/cross_unittest.d
@@ -17,14 +17,15 @@ unittest {
 
   // unit test all_true_geno for BC, autosome
   auto bc = form_cross("BC");
-  auto atg = bc.all_true_geno(false);
+  auto atg = bc.all_true_geno_A;
   assert(atg.length == 2);
   assert(atg[0] == new TrueGenotype(0,0));
   assert(atg[1] == new TrueGenotype(1,0));
 
+  // blank stuff for autosome
   auto is_X_chr = false;
   auto is_female = false;
-  auto cross_direction = new int[][](1,1);
+  auto cross_direction = new int[](1);
 
   // unit test init for BC
   foreach(gv; atg)
@@ -74,8 +75,8 @@ unittest {
   // unit test allTrueGeno for F2
   auto f2 = form_cross("F2");
   auto f2PK = form_cross_phaseknown(f2);
-  auto g = f2.all_true_geno;
-  auto gPK = f2PK.all_true_geno;
+  auto g = f2.all_true_geno_A;
+  auto gPK = f2PK.all_true_geno_A;
 
   assert(g.length == 3);
   assert(g[0] == new TrueGenotype(0,0));
@@ -89,76 +90,81 @@ unittest {
   assert(gPK[2] == new TrueGenotype(0,1));
   assert(gPK[3] == new TrueGenotype(1,1));
 
+  // blank stuff for autosome
+  auto is_X_chr = false;
+  auto is_female = false;
+  auto cross_direction = new int[](1);
+
   // unit test init for F2
   double val[] = [log(0.25), log(0.5), log(0.25)];
 
   foreach(i, gv; g)
-    assert(to!string(f2.init(gv)) == to!string(val[i]));
+    assert(to!string(f2.init(gv, is_X_chr, is_female, cross_direction)) == to!string(val[i]));
 
   // unit test init for F2 (phase-known)
   foreach(gv; gPK)
-    assert(to!string(f2PK.init(gv)) == to!string(val[0]));
+    assert(to!string(f2PK.init(gv, is_X_chr, is_female, cross_direction)) == to!string(val[0]));
 
   //  unit test step for F2
   double rf = 0.01;
 
-  assert( to!string( f2.step(g[0], g[0], rf) ) ==
+  assert( to!string( f2.step(g[0], g[0], rf, is_X_chr, is_female, cross_direction) ) ==
           to!string( log((1-rf)^^2) ));
-  assert( to!string( f2.step(g[0], g[1], rf) ) ==
+  assert( to!string( f2.step(g[0], g[1], rf, is_X_chr, is_female, cross_direction) ) ==
           to!string( log(2.0*rf*(1-rf)) ));
-  assert( to!string( f2.step(g[0], g[2], rf) ) ==
+  assert( to!string( f2.step(g[0], g[2], rf, is_X_chr, is_female, cross_direction) ) ==
           to!string( log(rf^^2) ));
 
-  assert( to!string( f2.step(g[1], g[0], rf) ) ==
+  assert( to!string( f2.step(g[1], g[0], rf, is_X_chr, is_female, cross_direction) ) ==
           to!string( log(rf*(1-rf)) ));
-  assert( to!string( f2.step(g[1], g[1], rf) ) ==
+  assert( to!string( f2.step(g[1], g[1], rf, is_X_chr, is_female, cross_direction) ) ==
           to!string( log(rf^^2 + (1-rf)^^2) ));
-  assert( to!string( f2.step(g[1], g[2], rf) ) ==
+  assert( to!string( f2.step(g[1], g[2], rf, is_X_chr, is_female, cross_direction) ) ==
           to!string( log(rf*(1-rf)) ));
 
-  assert( to!string( f2.step(g[2], g[0], rf) ) ==
+  assert( to!string( f2.step(g[2], g[0], rf, is_X_chr, is_female, cross_direction) ) ==
           to!string( log(rf^^2) ));
-  assert( to!string( f2.step(g[2], g[1], rf) ) ==
+  assert( to!string( f2.step(g[2], g[1], rf, is_X_chr, is_female, cross_direction) ) ==
           to!string( log(2.0*rf*(1-rf)) ));
-  assert( to!string( f2.step(g[2], g[2], rf) ) ==
+  assert( to!string( f2.step(g[2], g[2], rf, is_X_chr, is_female, cross_direction) ) ==
           to!string( log((1-rf)^^2) ));
 
 
   // unit test step for F2 (phase-known)
-  assert( to!string( f2PK.step(gPK[0], gPK[0], rf) ) ==
+  assert( to!string( f2PK.step(gPK[0], gPK[0], rf, is_X_chr, is_female, cross_direction) ) ==
           to!string( log((1-rf)^^2) ));
-  assert( to!string( f2PK.step(gPK[0], gPK[1], rf) ) ==
+  assert( to!string( f2PK.step(gPK[0], gPK[1], rf, is_X_chr, is_female, cross_direction) ) ==
           to!string( log(rf*(1-rf)) ));
-  assert( to!string( f2PK.step(gPK[0], gPK[2], rf) ) ==
+  assert( to!string( f2PK.step(gPK[0], gPK[2], rf, is_X_chr, is_female, cross_direction) ) ==
           to!string( log(rf*(1-rf)) ));
-  assert( to!string( f2PK.step(gPK[0], gPK[3], rf) ) ==
+  assert( to!string( f2PK.step(gPK[0], gPK[3], rf, is_X_chr, is_female, cross_direction) ) ==
           to!string( log(rf^^2) ));
 
-  assert( to!string( f2PK.step(gPK[1], gPK[0], rf) ) ==
+  assert( to!string( f2PK.step(gPK[1], gPK[0], rf, is_X_chr, is_female, cross_direction) ) ==
           to!string( log(rf*(1-rf)) ));
-  assert( to!string( f2PK.step(gPK[1], gPK[1], rf) ) ==
+  assert( to!string( f2PK.step(gPK[1], gPK[1], rf, is_X_chr, is_female, cross_direction) ) ==
           to!string( log((1-rf)^^2) ));
-  assert( to!string( f2PK.step(gPK[1], gPK[2], rf) ) ==
+  assert( to!string( f2PK.step(gPK[1], gPK[2], rf, is_X_chr, is_female, cross_direction) ) ==
           to!string( log(rf^^2) ));
-  assert( to!string( f2PK.step(gPK[1], gPK[3], rf) ) ==
+  assert( to!string( f2PK.step(gPK[1], gPK[3], rf, is_X_chr, is_female, cross_direction) ) ==
           to!string( log(rf*(1.0-rf)) ));
 
-  assert( to!string( f2PK.step(gPK[2], gPK[0], rf) ) ==
+  assert( to!string( f2PK.step(gPK[2], gPK[0], rf, is_X_chr, is_female, cross_direction) ) ==
           to!string( log(rf*(1-rf)) ));
-  assert( to!string( f2PK.step(gPK[2], gPK[2], rf) ) ==
+  assert( to!string( f2PK.step(gPK[2], gPK[2], rf, is_X_chr, is_female, cross_direction) ) ==
           to!string( log((1-rf)^^2) ));
-  assert( to!string( f2PK.step(gPK[2], gPK[1], rf) ) ==
+  assert( to!string( f2PK.step(gPK[2], gPK[1], rf, is_X_chr, is_female, cross_direction) ) ==
           to!string( log(rf^^2) ));
-  assert( to!string( f2PK.step(gPK[2], gPK[3], rf) ) ==
+  assert( to!string( f2PK.step(gPK[2], gPK[3], rf, is_X_chr, is_female, cross_direction) ) ==
           to!string( log(rf*(1.0-rf)) ));
 
-  assert( to!string( f2PK.step(gPK[3], gPK[3], rf) ) ==
+  assert( to!string( f2PK.step(gPK[3], gPK[3], rf, is_X_chr, is_female, cross_direction) ) ==
           to!string( log((1-rf)^^2) ));
-  assert( to!string( f2PK.step(gPK[3], gPK[1], rf) ) ==
+  assert( to!string( f2PK.step(gPK[3], gPK[1], rf, is_X_chr, is_female, cross_direction) ) ==
           to!string( log(rf*(1-rf)) ));
-  assert( to!string( f2PK.step(gPK[3], gPK[2], rf) ) ==
+  assert( to!string( f2PK.step(gPK[3], gPK[2], rf, is_X_chr, is_female, cross_direction) ) ==
           to!string( log(rf*(1-rf)) ));
-  assert( to!string( f2PK.step(gPK[3], gPK[0], rf) ) ==
+  assert( to!string( f2PK.step(gPK[3], gPK[0], rf, is_X_chr, is_female, cross_direction) ) ==
           to!string( log(rf^^2) ));
 
   // unit test emit for F2
@@ -181,78 +187,79 @@ unittest {
 
   double error_prob = 0.01;
 
-  assert(f2.emit(NA, g[0], error_prob) == 0);
-  assert(f2.emit(NA, g[1], error_prob) == 0);
-  assert(f2.emit(NA, g[2], error_prob) == 0);
+  assert(f2.emit(NA, g[0], error_prob, is_X_chr, is_female, cross_direction) == 0);
+  assert(f2.emit(NA, g[1], error_prob, is_X_chr, is_female, cross_direction) == 0);
+  assert(f2.emit(NA, g[2], error_prob, is_X_chr, is_female, cross_direction) == 0);
 
-  assert(to!string(f2.emit(A, g[0], error_prob)) == to!string(log(1.0-error_prob)));
-  assert(to!string(f2.emit(A, g[1], error_prob)) == to!string(log(error_prob/2.0)));
-  assert(to!string(f2.emit(A, g[2], error_prob)) == to!string(log(error_prob/2.0)));
+  assert(to!string(f2.emit(A, g[0], error_prob, is_X_chr, is_female, cross_direction))
+         == to!string(log(1.0-error_prob)));
+  assert(to!string(f2.emit(A, g[1], error_prob, is_X_chr, is_female, cross_direction)) == to!string(log(error_prob/2.0)));
+  assert(to!string(f2.emit(A, g[2], error_prob, is_X_chr, is_female, cross_direction)) == to!string(log(error_prob/2.0)));
 
-  assert(to!string(f2.emit(H, g[1], error_prob)) == to!string(log(1.0-error_prob)));
-  assert(to!string(f2.emit(H, g[0], error_prob)) == to!string(log(error_prob/2.0)));
-  assert(to!string(f2.emit(H, g[2], error_prob)) == to!string(log(error_prob/2.0)));
+  assert(to!string(f2.emit(H, g[1], error_prob, is_X_chr, is_female, cross_direction)) == to!string(log(1.0-error_prob)));
+  assert(to!string(f2.emit(H, g[0], error_prob, is_X_chr, is_female, cross_direction)) == to!string(log(error_prob/2.0)));
+  assert(to!string(f2.emit(H, g[2], error_prob, is_X_chr, is_female, cross_direction)) == to!string(log(error_prob/2.0)));
 
-  assert(to!string(f2.emit(B, g[2], error_prob)) == to!string(log(1.0-error_prob)));
-  assert(to!string(f2.emit(B, g[0], error_prob)) == to!string(log(error_prob/2.0)));
-  assert(to!string(f2.emit(B, g[1], error_prob)) == to!string(log(error_prob/2.0)));
+  assert(to!string(f2.emit(B, g[2], error_prob, is_X_chr, is_female, cross_direction)) == to!string(log(1.0-error_prob)));
+  assert(to!string(f2.emit(B, g[0], error_prob, is_X_chr, is_female, cross_direction)) == to!string(log(error_prob/2.0)));
+  assert(to!string(f2.emit(B, g[1], error_prob, is_X_chr, is_female, cross_direction)) == to!string(log(error_prob/2.0)));
 
-  assert(to!string(f2.emit(AorH, g[0], error_prob)) == to!string(log(1.0-error_prob/2.0)));
-  assert(to!string(f2.emit(AorH, g[1], error_prob)) == to!string(log(1.0-error_prob/2.0)));
-  assert(to!string(f2.emit(AorH, g[2], error_prob)) == to!string(log(error_prob)));
+  assert(to!string(f2.emit(AorH, g[0], error_prob, is_X_chr, is_female, cross_direction)) == to!string(log(1.0-error_prob/2.0)));
+  assert(to!string(f2.emit(AorH, g[1], error_prob, is_X_chr, is_female, cross_direction)) == to!string(log(1.0-error_prob/2.0)));
+  assert(to!string(f2.emit(AorH, g[2], error_prob, is_X_chr, is_female, cross_direction)) == to!string(log(error_prob)));
 
-  assert(to!string(f2.emit(HorB, g[0], error_prob)) == to!string(log(error_prob)));
-  assert(to!string(f2.emit(HorB, g[1], error_prob)) == to!string(log(1.0-error_prob/2.0)));
-  assert(to!string(f2.emit(HorB, g[2], error_prob)) == to!string(log(1.0-error_prob/2.0)));
+  assert(to!string(f2.emit(HorB, g[0], error_prob, is_X_chr, is_female, cross_direction)) == to!string(log(error_prob)));
+  assert(to!string(f2.emit(HorB, g[1], error_prob, is_X_chr, is_female, cross_direction)) == to!string(log(1.0-error_prob/2.0)));
+  assert(to!string(f2.emit(HorB, g[2], error_prob, is_X_chr, is_female, cross_direction)) == to!string(log(1.0-error_prob/2.0)));
 
-  assert(f2PK.emit(NA, gPK[0], error_prob) == 0);
-  assert(f2PK.emit(NA, gPK[1], error_prob) == 0);
-  assert(f2PK.emit(NA, gPK[2], error_prob) == 0);
-  assert(f2PK.emit(NA, gPK[3], error_prob) == 0);
+  assert(f2PK.emit(NA, gPK[0], error_prob, is_X_chr, is_female, cross_direction) == 0);
+  assert(f2PK.emit(NA, gPK[1], error_prob, is_X_chr, is_female, cross_direction) == 0);
+  assert(f2PK.emit(NA, gPK[2], error_prob, is_X_chr, is_female, cross_direction) == 0);
+  assert(f2PK.emit(NA, gPK[3], error_prob, is_X_chr, is_female, cross_direction) == 0);
 
-  assert(to!string(f2PK.emit(A, gPK[0], error_prob)) == to!string(log(1.0-error_prob)));
-  assert(to!string(f2PK.emit(A, gPK[1], error_prob)) == to!string(log(error_prob/2.0)));
-  assert(to!string(f2PK.emit(A, gPK[2], error_prob)) == to!string(log(error_prob/2.0)));
-  assert(to!string(f2PK.emit(A, gPK[3], error_prob)) == to!string(log(error_prob/2.0)));
+  assert(to!string(f2PK.emit(A, gPK[0], error_prob, is_X_chr, is_female, cross_direction)) == to!string(log(1.0-error_prob)));
+  assert(to!string(f2PK.emit(A, gPK[1], error_prob, is_X_chr, is_female, cross_direction)) == to!string(log(error_prob/2.0)));
+  assert(to!string(f2PK.emit(A, gPK[2], error_prob, is_X_chr, is_female, cross_direction)) == to!string(log(error_prob/2.0)));
+  assert(to!string(f2PK.emit(A, gPK[3], error_prob, is_X_chr, is_female, cross_direction)) == to!string(log(error_prob/2.0)));
 
-  assert(to!string(f2PK.emit(H, gPK[1], error_prob)) == to!string(log(1.0-error_prob)));
-  assert(to!string(f2PK.emit(H, gPK[2], error_prob)) == to!string(log(1.0-error_prob)));
-  assert(to!string(f2PK.emit(H, gPK[0], error_prob)) == to!string(log(error_prob/2.0)));
-  assert(to!string(f2PK.emit(H, gPK[3], error_prob)) == to!string(log(error_prob/2.0)));
+  assert(to!string(f2PK.emit(H, gPK[1], error_prob, is_X_chr, is_female, cross_direction)) == to!string(log(1.0-error_prob)));
+  assert(to!string(f2PK.emit(H, gPK[2], error_prob, is_X_chr, is_female, cross_direction)) == to!string(log(1.0-error_prob)));
+  assert(to!string(f2PK.emit(H, gPK[0], error_prob, is_X_chr, is_female, cross_direction)) == to!string(log(error_prob/2.0)));
+  assert(to!string(f2PK.emit(H, gPK[3], error_prob, is_X_chr, is_female, cross_direction)) == to!string(log(error_prob/2.0)));
 
-  assert(to!string(f2PK.emit(B, gPK[3], error_prob)) == to!string(log(1.0-error_prob)));
-  assert(to!string(f2PK.emit(B, gPK[0], error_prob)) == to!string(log(error_prob/2.0)));
-  assert(to!string(f2PK.emit(B, gPK[1], error_prob)) == to!string(log(error_prob/2.0)));
-  assert(to!string(f2PK.emit(B, gPK[2], error_prob)) == to!string(log(error_prob/2.0)));
+  assert(to!string(f2PK.emit(B, gPK[3], error_prob, is_X_chr, is_female, cross_direction)) == to!string(log(1.0-error_prob)));
+  assert(to!string(f2PK.emit(B, gPK[0], error_prob, is_X_chr, is_female, cross_direction)) == to!string(log(error_prob/2.0)));
+  assert(to!string(f2PK.emit(B, gPK[1], error_prob, is_X_chr, is_female, cross_direction)) == to!string(log(error_prob/2.0)));
+  assert(to!string(f2PK.emit(B, gPK[2], error_prob, is_X_chr, is_female, cross_direction)) == to!string(log(error_prob/2.0)));
 
-  assert(to!string(f2PK.emit(AorH, gPK[0], error_prob)) == to!string(log(1.0-error_prob/2.0)));
-  assert(to!string(f2PK.emit(AorH, gPK[1], error_prob)) == to!string(log(1.0-error_prob/2.0)));
-  assert(to!string(f2PK.emit(AorH, gPK[2], error_prob)) == to!string(log(1.0-error_prob/2.0)));
-  assert(to!string(f2PK.emit(AorH, gPK[3], error_prob)) == to!string(log(error_prob)));
+  assert(to!string(f2PK.emit(AorH, gPK[0], error_prob, is_X_chr, is_female, cross_direction)) == to!string(log(1.0-error_prob/2.0)));
+  assert(to!string(f2PK.emit(AorH, gPK[1], error_prob, is_X_chr, is_female, cross_direction)) == to!string(log(1.0-error_prob/2.0)));
+  assert(to!string(f2PK.emit(AorH, gPK[2], error_prob, is_X_chr, is_female, cross_direction)) == to!string(log(1.0-error_prob/2.0)));
+  assert(to!string(f2PK.emit(AorH, gPK[3], error_prob, is_X_chr, is_female, cross_direction)) == to!string(log(error_prob)));
 
-  assert(to!string(f2PK.emit(HorB, gPK[0], error_prob)) == to!string(log(error_prob)));
-  assert(to!string(f2PK.emit(HorB, gPK[1], error_prob)) == to!string(log(1.0-error_prob/2.0)));
-  assert(to!string(f2PK.emit(HorB, gPK[2], error_prob)) == to!string(log(1.0-error_prob/2.0)));
-  assert(to!string(f2PK.emit(HorB, gPK[3], error_prob)) == to!string(log(1.0-error_prob/2.0)));
+  assert(to!string(f2PK.emit(HorB, gPK[0], error_prob, is_X_chr, is_female, cross_direction)) == to!string(log(error_prob)));
+  assert(to!string(f2PK.emit(HorB, gPK[1], error_prob, is_X_chr, is_female, cross_direction)) == to!string(log(1.0-error_prob/2.0)));
+  assert(to!string(f2PK.emit(HorB, gPK[2], error_prob, is_X_chr, is_female, cross_direction)) == to!string(log(1.0-error_prob/2.0)));
+  assert(to!string(f2PK.emit(HorB, gPK[3], error_prob, is_X_chr, is_female, cross_direction)) == to!string(log(1.0-error_prob/2.0)));
 
   // unit test nrec for F2 (phase-known)
-  assert( f2PK.nrec(gPK[0], gPK[0]) == 0.0 );
-  assert( f2PK.nrec(gPK[0], gPK[1]) == 0.5 );
-  assert( f2PK.nrec(gPK[0], gPK[2]) == 0.5 );
-  assert( f2PK.nrec(gPK[0], gPK[3]) == 1.0 );
+  assert( f2PK.nrec(gPK[0], gPK[0], is_X_chr, is_female, cross_direction) == 0.0 );
+  assert( f2PK.nrec(gPK[0], gPK[1], is_X_chr, is_female, cross_direction) == 0.5 );
+  assert( f2PK.nrec(gPK[0], gPK[2], is_X_chr, is_female, cross_direction) == 0.5 );
+  assert( f2PK.nrec(gPK[0], gPK[3], is_X_chr, is_female, cross_direction) == 1.0 );
 
-  assert( f2PK.nrec(gPK[1], gPK[0]) == 0.5 );
-  assert( f2PK.nrec(gPK[1], gPK[1]) == 0.0 );
-  assert( f2PK.nrec(gPK[1], gPK[2]) == 1.0 );
-  assert( f2PK.nrec(gPK[1], gPK[3]) == 0.5 );
+  assert( f2PK.nrec(gPK[1], gPK[0], is_X_chr, is_female, cross_direction) == 0.5 );
+  assert( f2PK.nrec(gPK[1], gPK[1], is_X_chr, is_female, cross_direction) == 0.0 );
+  assert( f2PK.nrec(gPK[1], gPK[2], is_X_chr, is_female, cross_direction) == 1.0 );
+  assert( f2PK.nrec(gPK[1], gPK[3], is_X_chr, is_female, cross_direction) == 0.5 );
 
-  assert( f2PK.nrec(gPK[2], gPK[0]) == 0.5 );
-  assert( f2PK.nrec(gPK[2], gPK[2]) == 0.0 );
-  assert( f2PK.nrec(gPK[2], gPK[1]) == 1.0 );
-  assert( f2PK.nrec(gPK[2], gPK[3]) == 0.5 );
+  assert( f2PK.nrec(gPK[2], gPK[0], is_X_chr, is_female, cross_direction) == 0.5 );
+  assert( f2PK.nrec(gPK[2], gPK[2], is_X_chr, is_female, cross_direction) == 0.0 );
+  assert( f2PK.nrec(gPK[2], gPK[1], is_X_chr, is_female, cross_direction) == 1.0 );
+  assert( f2PK.nrec(gPK[2], gPK[3], is_X_chr, is_female, cross_direction) == 0.5 );
 
-  assert( f2PK.nrec(gPK[3], gPK[3]) == 0.0 );
-  assert( f2PK.nrec(gPK[3], gPK[1]) == 0.5 );
-  assert( f2PK.nrec(gPK[3], gPK[2]) == 0.5 );
-  assert( f2PK.nrec(gPK[3], gPK[0]) == 1.0 );
+  assert( f2PK.nrec(gPK[3], gPK[3], is_X_chr, is_female, cross_direction) == 0.0 );
+  assert( f2PK.nrec(gPK[3], gPK[1], is_X_chr, is_female, cross_direction) == 0.5 );
+  assert( f2PK.nrec(gPK[3], gPK[2], is_X_chr, is_female, cross_direction) == 0.5 );
+  assert( f2PK.nrec(gPK[3], gPK[0], is_X_chr, is_female, cross_direction) == 1.0 );
 }
