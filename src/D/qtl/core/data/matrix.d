572c69915026cc8e4da6587342d55bfcfb0a81f8
Pjotr Prins <pjotr.public01@thebird.nl>
2013-06-11 21:53:03 +0200

    Tuple version added - needs test

--- src/D/qtl/core/data/matrix.d
+++ src/D/qtl/core/data/matrix.d
@@ -12,21 +12,36 @@ import std.typecons;
 import std.algorithm;
 
 /**
- * Return the rows matching the test function on row elements
+ * Return those rows with index that match the test function on row elements (non-lazy)
+ * FIXME: write test and use line operator
+ */
+
+auto filter_matrix_by_row_with_index(T)(T[][] matrix, bool function (T) test ) {
+  uint i=0;
+  return map!( (row) { 
+    foreach(item; row) { 
+      if (!test(item)) return null; 
+      i += 1;
+    }
+    return tuple(i,row);
+  })(matrix).remove(null).array();
+}
+
+/**
+ * Return the rows matching the test function on row elements (non-lazy)
+ * FIXME: write test and use line operator
  */
 
 T[] filter_matrix_by_row(T)(T[][] matrix, bool function (T) test ) {
   return map!( (row) { 
-    foreach(item; row) {
-      if (!test(item))
-        return null;
-    }
+    foreach(item; row) { if (!test(item)) return null; }
     return row;
-  })(matrix).array();
+  })(matrix).remove(null).array();
 }
 
 /**
  * Return a list of booleans for rows matching the test function for every item in the row
+ * (non-lazy)
  */
 
 bool[] filter_matrix_by_row_2bool(T)(T[][] matrix, bool function (T) test ) {
