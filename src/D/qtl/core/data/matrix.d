e0892249b74ed5875b61ceb42adf9f57dd14c5ca
Pjotr Prins <pjotr.public01@thebird.nl>
2013-06-12 08:20:39 +0200

    Unit test for filter_matrix_by_row

--- src/D/qtl/core/data/matrix.d
+++ src/D/qtl/core/data/matrix.d
@@ -32,11 +32,21 @@ auto filter_matrix_by_row_with_index(T)(T[][] matrix, bool function (T) test ) {
  * FIXME: write test and use line operator
  */
 
-T[] filter_matrix_by_row(T)(T[][] matrix, bool function (T) test ) {
-  return map!( (row) { 
+auto filter_matrix_by_row(T)(T[][] matrix, bool function (T) test ) {
+  return remove!"a==[]"(map!( (row) { 
     foreach(item; row) { if (!test(item)) return null; }
     return row;
-  })(matrix).remove(null).array();
+  })(matrix).array());
+}
+
+unittest {
+  auto matrix = new double[][](3,3);
+  matrix.length = 3;
+  matrix[0] = [1.0,1.0,1.0];  
+  matrix[1] = [1.0,0.0,1.0];  
+  matrix[2] = [1.0,1.0,1.0];  
+  auto rows = filter_matrix_by_row!double(matrix, (item) => item==1.0);
+  assert(rows.length == 2,to!string(rows.length));
 }
 
 /**
@@ -48,3 +58,5 @@ bool[] filter_matrix_by_row_2bool(T)(T[][] matrix, bool function (T) test ) {
   return map!( (row) { return reduce!( (count,item) => count+test(item) )(0,row) > 0 ; } )(matrix).array();
 
 }
+
+
