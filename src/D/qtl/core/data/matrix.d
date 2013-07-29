d7e1a92a4c963eb2c9a055ea073c5382ba411acf
Karl Broman <kbroman@gmail.com>
2013-07-29 13:04:08 -0500

    Add function to take transpose of matrix

--- src/D/qtl/core/data/matrix.d
+++ src/D/qtl/core/data/matrix.d
@@ -11,6 +11,9 @@ import std.array;
 import std.typecons;
 import std.algorithm;
 
+import qtl.core.phenotype;
+
+
 /**
  * Return a list of booleans for rows matching the test function for every item in the row
  */
@@ -18,4 +21,51 @@ import std.algorithm;
 bool[] filter_matrix_by_row_2bool(T)(T[][] matrix, bool delegate (T) test ) {
   return map!( (row) { return reduce!( (count,item) => count+test(item) )(0,row) > 0 ; } )(matrix).array();
 
+}
+
+/**
+ * transpose of a matrix
+ */
+T[][] transpose_matrix(T)(in T[][] matrix)
+{
+  auto ret = new T[][](matrix[0].length, matrix.length);
+
+  foreach(i, row; matrix) {
+    foreach(j, element; row)
+      ret[j][i] = element;
+  }
+
+  return ret;
+}
+
+
+unittest {
+  writeln("Test transpose of phenotype matrix");
+
+  Phenotype pheno[][];
+  auto pdbl = [ ["0.0", "0.0", "0.0", "0.0", "0.0"],
+                ["0.0", "0.0", "0.0", "0.0", "0.0"],
+                [  "-", "0.0", "0.0",   "-", "0.0"],
+                ["0.0", "0.0", "0.0", "0.0", "0.0"],
+                ["0.0", "0.0", "0.0", "0.0", "0.0"],
+                ["0.0", "0.0", "0.0", "0.0", "0.0"],
+                [  "-",   "-",   "-",   "-",   "-"],
+                ["0.0", "0.0", "0.0", "0.0", "0.0"],
+                [  "-",   "-", "0.0",   "-",   "-"],
+                ["0.0", "0.0", "0.0", "0.0", "0.0"],
+                ["0.0",   "-",   "-", "0.0",   "-"],
+                ["0.0",   "-",   "-", "0.0",   "-"],
+                ["0.0", "0.0", "0.0", "0.0", "0.0"]];
+
+  foreach(line; pdbl) {
+    Phenotype[] ps = std.array.array(map!((a) {return
+    set_phenotype(a);})(line));
+    pheno ~= ps;
+  }
+
+  auto tpheno = transpose_matrix(pheno);
+
+  foreach(i, row; tpheno)
+    foreach(j, element; row)
+      assert(element == pheno[j][i]);
 }
