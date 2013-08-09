3ba199c67fa0ca10508c0085669221f75a09cf2e
Karl Broman <kbroman@gmail.com>
2013-08-09 13:07:52 -0500

    Remove trailing whitespace

--- src/D/qtl/core/data/matrix.d
+++ src/D/qtl/core/data/matrix.d
@@ -1,9 +1,9 @@
 /**
- * Generic matrix functions 
+ * Generic matrix functions
  *
  * These functions are considered standard algorithms for qtlHD and most of them
- * return lazy ranges. Some come with non-lazy alternatives to make type checking a 
- * little easier. 
+ * return lazy ranges. Some come with non-lazy alternatives to make type checking a
+ * little easier.
  *
  * For laziness in D see
  *
@@ -22,16 +22,16 @@ import std.typecons;
 import std.algorithm;
 import std.range;
 
-/** 
+/**
  * Test each row of a matrix and return the test result, the index and the row
- * element in a tuple. This is the full implementation that is lazy and can apply 
+ * element in a tuple. This is the full implementation that is lazy and can apply
  * to almost all situations.
  */
 
 auto test_matrix_by_row(T)(T[][] matrix, bool function (T[]) test_row ) {
   uint i=0;
-  return map!( (row) { 
-    return tuple(test_row(row),i++,row); 
+  return map!( (row) {
+    return tuple(test_row(row),i++,row);
   })(matrix);
 }
 
@@ -39,7 +39,7 @@ auto test_matrix_by_row_element(T)(T[][] matrix, bool function (T) test_element 
   // return test_matrix_by_row!T(matrix, row => true );
   // return test_matrix_by_row!T(matrix, (row) { return true; } );
   /* This should work, but does not
-  return test_matrix_by_row!T(matrix, (row) { 
+  return test_matrix_by_row!T(matrix, (row) {
     foreach(element ; row) {
       if (!test2(element)) return false;
     }
@@ -47,18 +47,18 @@ auto test_matrix_by_row_element(T)(T[][] matrix, bool function (T) test_element 
   });
   */
   uint i=0;
-  return map!( (row) { 
+  return map!( (row) {
     i += 1;
-    foreach(element; row) { 
+    foreach(element; row) {
       // breaks early when element test is false (no need to test the rest)
-      if (!test_element(element)) return tuple(false,i-1,row); 
+      if (!test_element(element)) return tuple(false,i-1,row);
     }
     return tuple(true,i-1,row);
   })(matrix);
 }
 
 /**
- * Return those rows with index that match the test function on row elements (non-lazy). 
+ * Return those rows with index that match the test function on row elements (non-lazy).
  *
  * For each matching row a tuple is returned containing the row index, and a pointer to
  * the row array.
@@ -88,9 +88,9 @@ T[][] non_lazy_filter_matrix_by_row(T)(T[][] matrix, bool function (T) test ) {
 unittest {
   auto matrix = new double[][](3,3);
   matrix.length = 3;
-  matrix[0] = [1.0,1.0,1.0];  
-  matrix[1] = [1.0,0.0,1.0];  
-  matrix[2] = [1.0,1.0,1.0];  
+  matrix[0] = [1.0,1.0,1.0];
+  matrix[1] = [1.0,0.0,1.0];
+  matrix[2] = [1.0,1.0,1.0];
   // writeln(matrix);
   // Test the matrix by row element
   auto rows1 = test_matrix_by_row_element!double(matrix, (item) => item==1.0).array();
@@ -107,10 +107,10 @@ unittest {
   auto tuples = non_lazy_filter_matrix_by_row_with_index!double(matrix, (item) => item==1.0);
   assert(tuples.length == 2,to!string(tuples.length));
   assert(tuples[1][1] == [1.0,1.0,1.0]);
-  // Test the matrix by row 
+  // Test the matrix by row
   auto rows2 = test_matrix_by_row!double(matrix, r => true );
   assert(rows2.length == 3,to!string(rows2.length));
-  auto rows3 = test_matrix_by_row!double(matrix, (r) { 
+  auto rows3 = test_matrix_by_row!double(matrix, (r) {
     return (reduce!"a+(b==1.0)"(0,r) == r.length); }
   ).array();
   assert(rows3.length == 3,to!string(rows3.length));
@@ -133,19 +133,19 @@ bool[] filter_matrix_by_row_2bool(T)(T[][] matrix, bool function (T) test ) {
 unittest {
   auto matrix = new double[][](3,3);
   matrix.length = 3;
-  matrix[0] = [1.0,1.0,1.0];  
-  matrix[1] = [1.0,0.0,1.0];  
-  matrix[2] = [1.0,1.0,1.0];  
+  matrix[0] = [1.0,1.0,1.0];
+  matrix[1] = [1.0,0.0,1.0];
+  matrix[2] = [1.0,1.0,1.0];
   auto rows = filter_matrix_by_row_2bool!double(matrix, item => item==1.0);
   assert(rows == [true,false,true],to!string(rows));
 }
 
-/** 
+/**
  * Test each column of a matrix and return the test result, the index and the column
- * element in a tuple. This is the full implementation that is lazy and can apply 
+ * element in a tuple. This is the full implementation that is lazy and can apply
  * to almost all situations.
  *
- * To create a lazy return value we loop with map. The column is returned as a 
+ * To create a lazy return value we loop with map. The column is returned as a
  * concrete array (if we make that lazy, it can get kinda crazy!)
  */
 
@@ -157,25 +157,25 @@ auto gen_t_matrix(T)(T[][] matrix) {
 
 auto test_matrix_by_col(T)(T[][] matrix, bool function (T[]) test_col ) {
   uint i = 0;
-  return map!( (col) { 
-    return tuple(test_col(col),i++,col); 
+  return map!( (col) {
+    return tuple(test_col(col),i++,col);
   })(gen_t_matrix(matrix));
 }
 
 auto test_matrix_by_col_element(T)(T[][] matrix, bool function (T) test_element ) {
   uint i=0;
-  return map!( (col) { 
+  return map!( (col) {
     i += 1;
-    foreach(element; col) { 
+    foreach(element; col) {
       // breaks early when element test is false (no need to test the rest)
-      if (!test_element(element)) return tuple(false,i-1,col); 
+      if (!test_element(element)) return tuple(false,i-1,col);
     }
     return tuple(true,i-1,col);
   })(gen_t_matrix(matrix));
 }
 
 T[][] non_lazy_filter_matrix_by_col(T)(T[][] matrix, bool function (T) test ) {
-  // the first element of each tuple is the bool - at this point the result 
+  // the first element of each tuple is the bool - at this point the result
   // is concrete, so as to not invoke the lambdas again. Maybe this can be
   // improved...
   auto range = filter!"a[0]"( test_matrix_by_col_element(matrix,test).array() );
@@ -198,9 +198,9 @@ unittest {
   writeln("Start column-wise");
   auto matrix = new double[][](3,3);
   matrix.length = 3;
-  matrix[0] = [1.0,1.0,1.0];  
-  matrix[1] = [1.0,0.0,1.0];  
-  matrix[2] = [1.0,2.0,1.0];  
+  matrix[0] = [1.0,1.0,1.0];
+  matrix[1] = [1.0,0.0,1.0];
+  matrix[2] = [1.0,2.0,1.0];
   // writeln(matrix);
   // Test the matrix by col element (all items in column should be 1.0)
   auto cols1 = test_matrix_by_col_element!double(matrix, (item) => item==1.0).array();
@@ -219,10 +219,10 @@ unittest {
   auto tuples = non_lazy_filter_matrix_by_col_with_index!double(matrix, (item) => item==1.0);
   assert(tuples.length == 2,to!string(tuples.length));
   assert(tuples[1][1] == [1.0,1.0,1.0]);
-  // Test the matrix by col 
+  // Test the matrix by col
   auto cols2 = test_matrix_by_col!double(matrix, r => true ).array();
   assert(cols2.length == 3,to!string(cols2.length));
-  auto cols3 = test_matrix_by_col!double(matrix, (r) { 
+  auto cols3 = test_matrix_by_col!double(matrix, (r) {
     return (reduce!"a+(b==1.0)"(0,r) == r.length); }
   ).array();
   assert(cols3.length == 3,to!string(cols3.length));
